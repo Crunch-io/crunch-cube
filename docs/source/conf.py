4e726a22ed5992ca32d55db89915d21b14f37aac
percious <chris@percious.com>
2017-10-20 07:44:30 -0600

    will this make it work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,11 +32,12 @@
 # ones.
 extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
-    'sphinx.ext.intersphinx',
+    #'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages']
+    #'sphinx.ext.githubpages'
+              ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
