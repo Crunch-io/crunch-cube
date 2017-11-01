690508eca8b09b415eccaef06d0d8fd2aed3da22
Slobodan Ilic <slobodan@crunch.io>
2017-11-01 18:05:24 +0100

    Additional fix for python 3 build

--- cr/cube/dimension.py
+++ cr/cube/dimension.py
@@ -31,12 +31,17 @@ class Dimension(object):
             return name
 
         # For numerical, datetime and text variables
-        name = element.get('value')
-        if isinstance(name, basestring):
-            return name
+        value = element.get('value')
+        # The following statement is used for compatibility between
+        # python 2 and 3. In python 3 everything is 'str' and 'unicode'
+        # is not defined. So, if the value is textual, in python 3 the first
+        # part of the 'or' statement should short-circuit to 'True'.
+        type_ = type(value)
+        if not type_ == dict and (type_ == str or type_ == unicode):
+            return value
 
         # For categorical array variables
-        name = name.get('references', {}).get('name')
+        name = value.get('references', {}).get('name')
         if name:
             return name
 
