de4a3d1f6329ea034f65a04a79d85448488ff0fe
Slobodan Ilic <slobodan@crunch.io>
2017-11-01 16:57:44 +0100

    Extend tests for labels

--- tests/unit/test_dimension.py
+++ tests/unit/test_dimension.py
@@ -45,7 +45,7 @@ class TestDimension(TestCase):
         actual = Dimension._get_type(dim)
         self.assertEqual(actual, expected)
 
-    def test_labels(self):
+    def test_labels_for_categoricals(self):
         name_cat_1 = Mock()
         name_cat_2 = Mock()
         name_cat_3 = Mock()
@@ -74,5 +74,42 @@ class TestDimension(TestCase):
         self.assertEqual(actual, expected)
         # Get all
         expected = [name_cat_1, name_cat_2, name_cat_3]
+        actual = Dimension(dim).labels(include_missing=True)
+        self.assertEqual(actual, expected)
+
+    def test_labels_for_numericals(self):
+        val_num_1 = 'fake val 1'
+        val_num_2 = 'fake val 2'
+        val_num_3 = Mock()
+        dim = {
+            'type': {
+                "subtype": {
+                    "class": "numeric"
+                },
+                "elements": [
+                    {
+                        "id": 0,
+                        "value": val_num_1,
+                        "missing": False,
+                    },
+                    {
+                        "id": 1,
+                        "value": val_num_2,
+                        "missing": False,
+                    },
+                    {
+                        "id": 2,
+                        "value": val_num_3,
+                        "missing": True,
+                    }
+                ],
+            }
+        }
+        # Get only non-missing
+        expected = [val_num_1, val_num_2]
+        actual = Dimension(dim).labels()
+        self.assertEqual(actual, expected)
+        # Get all
+        expected = [val_num_1, val_num_2, '<NA>']
         actual = Dimension(dim).labels(include_missing=True)
         self.assertEqual(actual, expected)
