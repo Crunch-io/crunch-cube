c5f4077c403158519bfa496ff685a43426a56daa
percious <chris@percious.com>
2017-11-07 13:04:38 -0700

    try napoleon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -37,7 +37,8 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
     #'sphinx.ext.githubpages'
-              ]
+    'sphinxcontrib.napoleon'
+  ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
