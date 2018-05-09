[#157391555]: Fix MR x CA margin calculation (as in whaam)

src/cr/cube/crunch_cube.py
@@ -536,28 +536,35 @@ class CrunchCube(object):
         )
         return table
 
-    def _mr_margin(self, axis, weighted, adjusted,
-                   include_transforms_for_dims=None, prune=False):
+    def _mr_margin(self, axis, weighted, adjusted, hs_dims=None, prune=False):
         '''Margin for cube that contains MR.'''
         if self.is_double_mr:
             return self._double_mr_margin(axis, weighted)
         elif self.ndim == 1:
             return self._1d_mr_margin(axis, weighted)
         elif self._calculate_along_non_mr(axis):
-            return self._mr_margin_along_non_mr_dim(axis, weighted,
-                                                    include_transforms_for_dims)
+            return self._mr_margin_along_non_mr_dim(axis, weighted, hs_dims)
+
+        is_ca_row_margin = (
+            self.ndim == 3 and
+            self.dimensions[1].type == 'categorical_array' and
+            axis is not None
+        )
+        if is_ca_row_margin:
+            # For MR x CA always return row margin (only one that makes sense)
+            return np.sum(self.as_array(), axis=2)
 
         table = self.table.data(weighted, margin=True)
-        if include_transforms_for_dims:
+        if hs_dims:
             # In case of H&S the entire table needs to be
             # transformed (with selections).
-            table = self._transform_table(table, include_transforms_for_dims)
+            table = self._transform_table(table, hs_dims)
 
         # For cases when the margin is calculated for the MR dimension, we need
         # the sum of selected and non-selected slices (if axis is None), or the
         # sublimated version (another sum along the axis), if axis is defined.
         margin = table[self.ind_selected] + table[self.ind_non_selected]
-        if not include_transforms_for_dims:
+        if not hs_dims:
             # If entire table was transformed, we already have it with all the
             # valid indices. If not, we need to apply valid indices.
             margin = margin[np.ix_(*self.valid_indices)]
@@ -654,7 +661,7 @@ class CrunchCube(object):
                 prune=prune,
             )
             if self.dimensions[1].type == 'categorical_array':
-                den = np.sum(self.as_array(), axis=2)[:, :, None]
+                den = self.margin(axis=2)[:, :, None]
             else:
                 den = self.margin(axis=1)[:, None, :]
             return num / den