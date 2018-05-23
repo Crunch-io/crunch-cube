599e83a518b72c5f2af0ee5b90cd39a22632617a
Slobodan Ilic <slobodan@crunch.io>
2018-05-23 10:50:12 +0200

    [#157772177]: Refactor and start adding functionality
    - Use composition instead of inheritance (which implicates that
      the calling party has to be aware of whether to use CubeSlice)
    - Start implementing methods and properties, driven by TDD

--- src/cr/cube/cube_slice.py
+++ src/cr/cube/cube_slice.py
@@ -2,10 +2,8 @@
 
 from __future__ import division
 
-from .crunch_cube import CrunchCube
 
-
-class CubeSlice(CrunchCube):
+class CubeSlice(object):
     '''Implementation of CubeSlice class.
 
     The CubeSlice is used to uniformly represent all cubes as 2D tables.
@@ -13,6 +11,51 @@ class CubeSlice(CrunchCube):
     achieved by slicing.
     '''
 
-    def __init__(self, response, index=None):
-        super(CubeSlice, self).__init__(response)
+    def __init__(self, cube, index=None):
+        self._cube = cube
         self._index = index
+
+    @property
+    def ndim(self):
+        '''Get number of dimensions.'''
+        return self._cube.ndim
+
+    @property
+    def name(self):
+        '''Get slice name.
+
+        In case of 2D return cube name. In case of 3D, return the combination
+        of the cube name with the label of the corresponding slice
+        (nth label of the 0th dimension).
+        '''
+        title = self._cube.name
+
+        if self.ndim < 3:
+            return title
+
+        table_name = self._cube.labels()[0][self._index]
+        return '%s: %s' % (title, table_name)
+
+    @property
+    def rows_title(self):
+        '''Get title of the rows dimension.
+
+        For 3D it's the 1st dimension (0th dimension of the current slice).
+        '''
+        return self._cube.dimensions[1].name
+
+    def as_array(self, *args, **kwargs):
+        '''Call cube's as_array, and return correct slice.'''
+        array = self._cube.as_array(*args, **kwargs)
+        return array if self._index is None else array[self._index]
+
+    @property
+    def inserted_rows_indices(self):
+        ''' Get correct inserted rows indices for the corresponding slice.
+
+        For 3D cubes, a list of tuples is returned from the cube, when invoking
+        the inserted_hs_indices method. The job of this property is to fetch
+        the correct tuple (the one corresponding to the current slice index),
+        and return the 0th value (the one corresponding to the rows).
+        '''
+        return self._cube.inserted_hs_indices()[self._index][0]
--- tests/unit/test_cube_slice.py
+++ tests/unit/test_cube_slice.py
@@ -1,28 +1,105 @@
 '''Unit tests for the CubeSlice class.'''
 from unittest import TestCase
 from mock import Mock
-from mock import patch
 
 from cr.cube.cube_slice import CubeSlice
 
 
 # pylint: disable=invalid-name, no-self-use, protected-access
-class TestCrunchCube(TestCase):
+class TestCubeSlice(TestCase):
     '''Test class for the CubeSlice unit tests.'''
 
-    @patch('cr.cube.crunch_cube.CrunchCube.__init__')
-    def test_init_no_index(self, mock_init):
+    def test_init_no_index(self):
         '''Test that init correctly invoked cube construction.'''
-        response = Mock()
-        cs = CubeSlice(response)
-        mock_init.assert_called_once_with(response)
+        cube = Mock()
+        cs = CubeSlice(cube)
+        assert cs._cube == cube
         assert cs._index is None
 
-    @patch('cr.cube.crunch_cube.CrunchCube.__init__')
-    def test_init_with_index(self, mock_init):
+    def test_init_with_index(self):
         '''Test that init correctly invoked cube construction and sets index.'''
-        response = Mock()
+        cube = Mock()
         index = Mock()
-        cs = CubeSlice(response, index)
-        mock_init.assert_called_once_with(response)
+        cs = CubeSlice(cube, index)
+        assert cs._cube == cube
         assert cs._index == index
+
+    def test_ndim_invokes_ndim_from_cube(self):
+        '''Test if ndim calls corresponding cube's method.'''
+        fake_ndim = Mock()
+        cube = Mock()
+        cube.ndim = fake_ndim
+
+        cs = CubeSlice(cube)
+        assert cs.ndim == fake_ndim
+
+    def test_name(self):
+        '''Test correct name is returned.
+
+        In case of 2D return cube name. In case of 3D, return the combination
+        of the cube name with the label of the corresponding slice
+        (nth label of the 0th dimension).
+        '''
+        # Assert name for <3D
+        fake_title = 'Cube Title'
+        cube = Mock()
+        cube.ndim = 2
+        cube.name = fake_title
+        cs = CubeSlice(cube)
+        assert cs.name == fake_title
+
+        # Assert name for 3D
+        fake_labels = [[Mock(), 'Analysis Slice XY', Mock()]]
+        cube.labels.return_value = fake_labels
+        cube.ndim = 3
+        cs = CubeSlice(cube, 1)
+        assert cs.name == 'Cube Title: Analysis Slice XY'
+
+    def test_as_array(self):
+        '''Test that as_array method delegetes its call to CrunchCube.
+
+        When the number of dimensions is smaller than 3, all the arguments
+        sould just be passed to the corresponding cube method, and the
+        result returned. When the number of dimensions is equal to 3, the
+        correct slice needs to be returned.
+        '''
+        cube = Mock()
+
+        # Assert arguments are passed correctly
+        cs = CubeSlice(cube)
+        arg = Mock()
+        kw_arg = Mock()
+        cs.as_array(arg, kw_arg=kw_arg)
+        cs._cube.as_array.assert_called_once_with(arg, kw_arg=kw_arg)
+
+        # Assert entire array is returned when index is not set
+        array = Mock()
+        cube.as_array.return_value = array
+        cs = CubeSlice(cube)
+        assert cs.as_array() == array
+
+        # Assert correct slice is returned when index is set
+        array = [Mock(), Mock(), Mock()]
+        cube.as_array.return_value = array
+        cs = CubeSlice(cube, index=1)
+        assert cs.as_array() == array[1]
+
+    def test_rows_title(self):
+        '''Assert correct rows title is returned.'''
+        cube = Mock()
+        cube.ndim = 3
+        cube.dimensions = [Mock(), Mock(), Mock()]
+        cube.dimensions[1].name = '1st Dimension Name'
+        cs = CubeSlice(cube, 1)
+        assert cs.rows_title == '1st Dimension Name'
+
+    def test_inserted_rows_indices(self):
+        '''Assert correct inserted row indices are returned.
+        '''
+        fake_indices = Mock()
+        cube = Mock()
+        cube.inserted_hs_indices.return_value = [
+            Mock(), (fake_indices, Mock()), Mock(),
+        ]
+        cs = CubeSlice(cube, 1)
+        assert cs.inserted_rows_indices == fake_indices
