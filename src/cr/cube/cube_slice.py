fc78f8ca72d1dfb754920175338bbe11f399c9b7
Slobodan Ilic <slobodan@crunch.io>
2018-05-29 17:50:17 +0200

    [#157772177]: Unify CubeSlice behavior for 2D and 3D cubes

--- src/cr/cube/cube_slice.py
+++ src/cr/cube/cube_slice.py
@@ -1,9 +1,5 @@
 '''Home of the CubeSlice class.'''
 
-from __future__ import division
-
-import numpy as np
-
 
 class CubeSlice(object):
     '''Implementation of CubeSlice class.
@@ -16,6 +12,35 @@ class CubeSlice(object):
     def __init__(self, cube, index):
         self._cube = cube
         self._index = index
+
+    def _update_args(self, kwargs):
+
+        if self.ndim < 3:
+            # If cube is 2D it doesn't actually have slices (itself is a slice).
+            # In this case we don't need to convert any arguments, but just
+            # pass them to the underlying cube (which is the slice).
+            return kwargs
+
+        axis = kwargs.get('axis')
+        if axis is None or isinstance(axis, tuple):
+            # If no axis was passed, we don't need to update anything. If axis
+            # was a tuple, it means that the caller was expecting a 3D cube.
+            return kwargs
+
+        kwargs['axis'] += 1
+        return kwargs
+
+    def _update_result(self, result):
+        if self.ndim < 3 or len(result) - 1 < self._index:
+            return result
+        return result[self._index]
+
+    def _call_cube_method(self, method, *args, **kwargs):
+        kwargs = self._update_args(kwargs)
+        result = getattr(self._cube, method)(*args, **kwargs)
+        return self._update_result(result)
+
+    # Properties
 
     @property
     def ndim(self):
@@ -46,32 +71,6 @@ class CubeSlice(object):
         '''
         return self._cube.dimensions[1].name
 
-    @staticmethod
-    def _increment_axis(axis):
-        if axis is not None and not isinstance(axis, tuple):
-            axis += 1
-        # return (1, 2)
-        return axis
-
-    def as_array(self, *args, **kwargs):
-        '''Call cube's as_array, and return correct slice.'''
-        array = self._cube.as_array(*args, **kwargs)
-        return array[self._index]
-
-    def proportions(self, *args, **kwargs):
-        '''Call cube's proportions, and return correct slice.'''
-        kwargs['axis'] = self._increment_axis(kwargs.get('axis'))
-        array = self._cube.proportions(*args, **kwargs)
-        return array[self._index]
-
-    def margin(self, *args, **kwargs):
-        '''Call cube's margin, and return correct slice.'''
-        kwargs['axis'] = self._increment_axis(kwargs.get('axis'))
-        margin = self._cube.margin(*args, **kwargs)
-        if len(margin) > 1:
-            return margin[self._index]
-        return margin
-
     @property
     def inserted_rows_indices(self):
         ''' Get correct inserted rows indices for the corresponding slice.
@@ -83,14 +82,6 @@ class CubeSlice(object):
         '''
         return self._cube.inserted_hs_indices()[self._index][0]
 
-    def labels(self, *args, **kwargs):
-        '''Return correct labels for slice.'''
-        return self._cube.labels(*args, **kwargs)[-2:]
-
-    def prune_indices(self, *args, **kwargs):
-        '''Extract correct row/col prune indices from 3D cube.'''
-        return list(self._cube.prune_indices(*args, **kwargs)[self._index])
-
     @property
     def has_means(self):
         '''Get has_means from cube.'''
@@ -98,4 +89,46 @@ class CubeSlice(object):
 
     @property
     def dimensions(self):
+        '''Get slice dimensions.
+
+        For 2D cubes just get their dimensions. For 3D cubes, don't get the
+        first dimension (only take slice dimensions).
+        '''
         return self._cube.dimensions[-2:]
+
+    # API Methods
+
+    def labels(self, *args, **kwargs):
+        '''Return correct labels for slice.'''
+        return self._cube.labels(*args, **kwargs)[-2:]
+
+    def prune_indices(self, *args, **kwargs):
+        '''Extract correct row/col prune indices from 3D cube.'''
+        if self.ndim < 3:
+            return self._cube.prune_indices(*args, **kwargs)
+        return list(self._cube.prune_indices(*args, **kwargs)[self._index])
+
+    def as_array(self, *args, **kwargs):
+        '''Call cube's as_array, and return correct slice.'''
+        return self._call_cube_method('as_array', *args, **kwargs)
+
+    def proportions(self, *args, **kwargs):
+        '''Call cube's proportions, and return correct slice.'''
+        return self._call_cube_method('proportions', *args, **kwargs)
+
+    def margin(self, *args, **kwargs):
+        '''Call cube's margin, and return correct slice.'''
+        return self._call_cube_method('margin', *args, **kwargs)
+
+    def population_counts(self, *args, **kwargs):
+        '''Get population counts.'''
+        return self._call_cube_method('population_counts', *args, **kwargs)
+
+    @property
+    def standardized_residuals(self):
+        '''Get cube's standardized residuals.'''
+        return self._cube.standardized_residuals
+
+    def index(self, *args, **kwargs):
+        '''Get index.'''
+        return self._call_cube_method('index', *args, **kwargs)
