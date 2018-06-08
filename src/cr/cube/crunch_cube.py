48abe4d76d14673105d8559fa1b106e52dbad15e
Slobodan Ilic <slobodan@crunch.io>
2018-06-08 13:58:23 +0200

    [#158185408]: Don't force-keep H&S if they're pruned in both dims

--- src/cr/cube/crunch_cube.py
+++ src/cr/cube/crunch_cube.py
@@ -210,13 +210,6 @@ class CrunchCube(DataTable):
             )
             slice_mask = np.logical_or(rows_pruned, cols_pruned)
 
-            # 0 stands for row dim (it's increased inside the method
-            inserted_rows_indices = self._inserted_dim_inds(transforms, 0)
-            if inserted_rows_indices.any():
-                hs_pruned = slice_mask[inserted_rows_indices, :].all(axis=1)
-                # make sure we never prune H&S
-                slice_mask[inserted_rows_indices[hs_pruned], :] = False
-
             # In case of MRs we need to "inflate" mask
             if self.mr_dim_ind == (1, 2):
                 slice_mask = slice_mask[:, np.newaxis, :, np.newaxis]
