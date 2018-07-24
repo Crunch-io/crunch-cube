af57370c47b232295bed465816f9b31c4fa49e74
Slobodan Ilic <slobodan@crunch.io>
2018-07-24 14:58:13 +0200

    Failing test for CA(it) x CA(cat) x CAT index

--- tests/integration/test_index.py
+++ tests/integration/test_index.py
@@ -7,6 +7,7 @@ from .fixtures import CAT_X_MR_SIMPLE
 from .fixtures import PETS_X_PETS
 from .fixtures import SELECTED_CROSSTAB_4
 from .fixtures import CAT_X_MR_X_MR
+from .fixtures import CA_ITEMS_X_CA_CAT_X_CAT
 
 
 class TestIndex(TestCase):
@@ -48,3 +49,46 @@ class TestIndex(TestCase):
         expected = np.array([])
         actual = cube.index()
         np.testing.assert_almost_equal(actual, expected)
+
+    def test_ca_items_x_ca_cat_x_cat_index(self):
+        cube = CrunchCube(CA_ITEMS_X_CA_CAT_X_CAT)
+
+        # Test index values
+        expected = np.array([
+            [[ 1.,  1., np.nan, np.nan, np.nan],
+             [np.nan, np.nan, np.nan, np.nan, np.nan],
+             [np.nan, np.nan, np.nan, np.nan, np.nan],
+             [np.nan, np.nan, np.nan, np.nan, np.nan]],
+
+            [[ 2.,  0., np.nan, np.nan, np.nan],
+             [ 0.,  2., np.nan, np.nan, np.nan],
+             [np.nan, np.nan, np.nan, np.nan, np.nan],
+             [np.nan, np.nan, np.nan, np.nan, np.nan]],
+
+            [[np.nan, np.nan, np.nan, np.nan, np.nan],
+             [ 2.,  0., np.nan, np.nan, np.nan],
+             [ 0.,  2., np.nan, np.nan, np.nan],
+             [np.nan, np.nan, np.nan, np.nan, np.nan]],
+        ])
+        actual = cube.index()
+        np.testing.assert_almost_equal(actual, expected)
+
+        # Test pruning mask
+        expected = np.array([
+            [[ False,  False, True, True, True],
+             [True, True, True, True, True],
+             [True, True, True, True, True],
+             [True, True, True, True, True]],
+
+            [[ False,  False, True, True, True],
+             [ False,  False, True, True, True],
+             [True, True, True, True, True],
+             [True, True, True, True, True]],
+
+            [[True, True, True, True, True],
+             [ False,  False, True, True, True],
+             [ False,  False, True, True, True],
+             [True, True, True, True, True]],
+        ])
+        actual = cube.index(prune=True).mask
+        np.testing.assert_array_equal(actual, expected)
