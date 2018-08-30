802ba10adf4a128154d5ac91f63dbd285a1c4023
Bert JW Regeer <bert@crunch.io>
2018-08-30 12:23:57 -0600

    Revert "Merge remote-tracking branch 'origin/rel-5.2.296'"
    This reverts commit 60dcc40b6981c8cd9a81a22d902d6153d21df3d9, reversing
    changes made to dbc12d8f6c05d4959011766a065103ca9dfd4f6a.

--- src/cr/cube/mixins/data_table.py
+++ src/cr/cube/mixins/data_table.py
@@ -132,12 +132,6 @@ class DataTable(object):
     def _shape(self):
         return tuple([dim.shape for dim in self.all_dimensions])
 
-    @lazyproperty
-    def counts(self):
-        unfiltered = self._cube['result'].get('unfiltered')
-        filtered = self._cube['result'].get('filtered')
-        return unfiltered, filtered
-
     def data(self, weighted, margin=False):
         '''Get the data in non-flattened shape.
 
