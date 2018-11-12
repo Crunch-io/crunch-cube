fix: add xfail for new broadcast Sentry error
* Add CubeSlice integration test that triggers "cannot broadcast..."
  error caught by Sentry.

* Silence W504 'line break after binary operator' flake8 error arising
  in latest version as those occur commonly in our code and we currently
  have no quick option to fix that.

* Remove slice reprs from CrunchCube repr as they are overwhelming and
  not useful when there is more than a couple small slices. Each slice
  can still be repr'ed individually on the command-line as desired.

* Rename MR_X_MR_X_CAT fixture to CAT_X_MR_X_MR_EMPTY_COLS to reflect
  its actual contents. Rename integration test that uses it similarly.

* Add new (actual) MR_X_MR_X_CAT fixture pulled from this error scenario
  and use it in new integration test that triggers this error.

tests/integration/test_cube_slice.py
@@ -1,9 +1,16 @@
+# encoding: utf-8
+
+"""Integration test suite for cr.cube.cube_slice module."""
+
+import numpy as np
+import pytest
+
 from cr.cube.crunch_cube import CrunchCube
 
 from ..fixtures import CR  # ---mnemonic: CR = 'cube-response'---
 
 
-class DescribeIntegratedCrunchCube(object):
+class DescribeIntegratedCubeSlice(object):
 
     def it_provides_a_console_friendly_repr_for_a_slice(self):
         cube = CrunchCube(CR.CAT_X_CAT)
@@ -17,6 +24,22 @@ class DescribeIntegratedCrunchCube(object):
             "--  ---  ---\n"
             "B     5    2\n"
             "C     5    3"
+        )
+
+    @pytest.mark.xfail(reason='WIP', strict=True)
+    def it_knows_zscore_for_MR_X_MR_X_CAT(self):
+        cube = CrunchCube(CR.MR_X_MR_X_CAT)
+        slice_ = cube.slices[0]
+
+        zscore = slice_.zscore()
+
+        np.testing.assert_array_almost_equal(
+            zscore,
+            # ---fix this expectation once we have plausible result---
+            np.array([
+                [1, 2, 3],
+                [4, 5, 6]
+            ])
         )
 
 

tests/integration/test_multiple_response.py
@@ -428,6 +428,32 @@ def test_cat_x_mr_x_mr_pruned_rows():
     np.testing.assert_array_equal(actual_1st_tab, expected_1st_tab)
 
 
+def test_cat_x_mr_x_mr_pruned_cols():
+    cube = CrunchCube(CR.CAT_X_MR_X_MR_EMPTY_COLS)
+
+    # Not pruned
+    expected = np.array([
+        [1.42180119, 5.67259693, 0.00000000, 0.],
+        [5.96105631, 1.46479350, 22.51724162, 0.],
+        [1.79182255, 1.19879791, 23.60391651, 0.],
+        [4.67364825, 8.02124010, 93.65643860, 0.],
+        [16.0107376, 13.06260544, 206.93592858, 0.],
+    ])
+    actual = cube.as_array()[0]
+    np.testing.assert_almost_equal(actual, expected)
+
+    # Pruned
+    expected = np.array([
+        [1.42180119, 5.67259693, 0.],
+        [5.96105631, 1.46479350, 22.51724162],
+        [1.79182255, 1.19879791, 23.60391651],
+        [4.67364825, 8.02124010, 93.65643860],
+        [16.0107376, 13.06260544, 206.93592858],
+    ])
+    actual = np.ma.compress_cols(cube.as_array(prune=True)[0])
+    np.testing.assert_almost_equal(actual, expected)
+
+
 def test_cat_x_mr_x_mr_proportions_by_col():
     cube = CrunchCube(CR.CAT_X_MR_X_MR)
     # Set axis to 1 (and not 0), since 3D cube
@@ -631,32 +657,6 @@ def test_mr_props_not_pruned():
         9.50985735e-04, 1.35477929e-01, 1.86531215e-01,
     ])
     actual = cube.proportions()
-    np.testing.assert_almost_equal(actual, expected)
-
-
-def test_mr_x_mr_x_cat_pruned_rows():
-    cube = CrunchCube(CR.MR_X_MR_X_CAT)
-
-    # Not pruned
-    expected = np.array([
-        [1.42180119, 5.67259693, 0.00000000, 0.],
-        [5.96105631, 1.46479350, 22.51724162, 0.],
-        [1.79182255, 1.19879791, 23.60391651, 0.],
-        [4.67364825, 8.02124010, 93.65643860, 0.],
-        [16.0107376, 13.06260544, 206.93592858, 0.],
-    ])
-    actual = cube.as_array()[0]
-    np.testing.assert_almost_equal(actual, expected)
-
-    # Pruned
-    expected = np.array([
-        [1.42180119, 5.67259693, 0.],
-        [5.96105631, 1.46479350, 22.51724162],
-        [1.79182255, 1.19879791, 23.60391651],
-        [4.67364825, 8.02124010, 93.65643860],
-        [16.0107376, 13.06260544, 206.93592858],
-    ])
-    actual = np.ma.compress_cols(cube.as_array(prune=True)[0])
     np.testing.assert_almost_equal(actual, expected)
 
 