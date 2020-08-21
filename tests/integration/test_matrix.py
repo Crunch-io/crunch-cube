22868f44dd4b9ca7546b8a2143aad36ef498776a
Steve Canny <stcanny@gmail.com>
2020-08-20 23:55:52 -0700

    xfail: XFAILs for _BaseInsertedVector.insertion_id

---
+++ tests/integration/test_matrix.py
@@ -0,0 +1,34 @@
+# encoding: utf-8
+
+"""Partial-integration test suite for `cr.cube.matrix` module."""
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import pytest
+
+from cr.cube.dimension import _Subtotal
+from cr.cube.matrix import _BaseInsertedVector
+
+
+class Describe_BaseInsertedVector(object):
+    """Unit test suite for `cr.cube.matrix._BaseInsertedVector` object."""
+
+    @pytest.mark.xfail(reason="WIP", raises=NotImplementedError, strict=True)
+    def it_knows_its_insertion_id_when_it_has_been_assigned_one(self):
+        subtotal_dict = {"insertion_id": 42}
+        subtotal = _Subtotal(subtotal_dict, None, None)
+        inserted_vector = _BaseInsertedVector(subtotal, None, None, None, None)
+
+        insertion_id = inserted_vector.insertion_id
+
+        assert insertion_id == 42
+
+    @pytest.mark.xfail(reason="WIP", raises=TypeError, strict=True)
+    def and_it_is_assigned_a_fallback_when_it_has_no_insertion_id(self):
+        subtotal_dict = {}
+        subtotal = _Subtotal(subtotal_dict, None, None, 24)
+        inserted_vector = _BaseInsertedVector(subtotal, None, None, None, None)
+
+        insertion_id = inserted_vector.insertion_id
+
+        assert insertion_id == 24
