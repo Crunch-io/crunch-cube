69d9641dd3574bc19553c19e0e3233e9f389cce8
Steve Canny <stcanny@gmail.com>
2020-08-21 00:03:04 -0700

    sort: add _BaseAnchoredCollator.display_order

---
+++ tests/unit/test_collator.py
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+"""Unit test suite for `cr.cube.collator` module."""
+
+import pytest
+
+from cr.cube.collator import _BaseAnchoredCollator
+from cr.cube.dimension import Dimension
+
+from ..unitutil import ANY, initializer_mock, instance_mock, property_mock
+
+
+class Describe_BaseAnchoredCollator(object):
+    """Unit-test suite for `cr.cube.collator._BaseAnchoredCollator` object."""
+
+    def it_provides_an_interface_classmethod(self, request, dimension_):
+        _init_ = initializer_mock(request, _BaseAnchoredCollator)
+        _display_order_ = property_mock(
+            request, _BaseAnchoredCollator, "_display_order"
+        )
+        _display_order_.return_value = (-3, 0, 1, -2, 2, 3, -1)
+
+        display_order = _BaseAnchoredCollator.display_order(dimension_, empty_idxs=(2,))
+
+        _init_.assert_called_once_with(ANY, dimension_, (2,))
+        assert display_order == (-3, 0, 1, -2, 2, 3, -1)
+
+    # fixture components ---------------------------------------------
+
+    @pytest.fixture
+    def dimension_(self, request):
+        return instance_mock(request, Dimension)
