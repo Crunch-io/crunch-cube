fccdd6d23950ce1127a1db36c03002a0c1f30fae
ernestoarbitrio <ernesto.arbitrio@gmail.com>
2020-09-21 11:11:13 +0200

    tests: parametrize test index table

--- tests/unit/test_index_table.py
+++ tests/unit/test_index_table.py
@@ -17,25 +17,9 @@ from ..unitutil import instance_mock
 
 
 class DescribeIndexTable(object):
-    def test_index_performs_correct_division(self, index_fixture, cube_):
-        proportions, axis, base, expected = index_fixture
-        cube_.dim_types = (DT.CAT, DT.CAT)
-        cube_.ndim = 2
-        cube_.mr_dim_ind = None
-        cube_.proportions.return_value = np.array(proportions)
-        cube_.as_array.return_value = np.array(proportions)
-        cube_slice = CubeSlice(cube_, 0)
-
-        actual = cube_slice.index_table(axis, base)
-
-        np.testing.assert_almost_equal(actual, expected)
-
-    @pytest.fixture
-    def cube_(self, request):
-        return instance_mock(request, CrunchCube)
-
-    @pytest.fixture(
-        params=[
+    @pytest.mark.parametrize(
+        "proportions, axis, base, expected",
+        (
             (
                 [[0.714285714286, 0.285714285714], [0.625, 0.375]],
                 1,
@@ -48,10 +32,24 @@ class DescribeIndexTable(object):
                 [0.6, 0.4],
                 [[83.3333333333333, 66.6666666666667], [125, 150]],
             ),
-        ]
+        ),
     )
-    def index_fixture(self, request):
-        proportions, axis, base, expected = request.param
-        # ---fixture only returns case data; combining that data into test-fixturing
-        #    happens up in the test itself to give better context when reading test---
-        return proportions, axis, np.array(base), expected
+    def test_index_performs_correct_division(
+        self, proportions, axis, base, expected, cube_
+    ):
+        cube_.dim_types = (DT.CAT, DT.CAT)
+        cube_.ndim = 2
+        cube_.mr_dim_ind = None
+        cube_.proportions.return_value = np.array(proportions)
+        cube_.as_array.return_value = np.array(proportions)
+        cube_slice = CubeSlice(cube_, 0)
+
+        actual = cube_slice.index_table(axis, np.array(base))
+
+        np.testing.assert_almost_equal(actual, expected)
+
+    # fixture components ---------------------------------------------
+
+    @pytest.fixture
+    def cube_(self, request):
+        return instance_mock(request, CrunchCube)
