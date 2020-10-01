49748c39a825810dc0521c7738149b5af77e1916
Slobodan Ilic <slobodan@crunch.io>
2020-10-01 15:46:11 +0200

    tests: num arr means with 1D grouping

--- tests/integration/test_numeric_array.py
+++ tests/integration/test_numeric_array.py
@@ -10,8 +10,30 @@ from cr.cube.cube import Cube
 
 from ..fixtures import NA
 
+NAN = np.nan
 
-class TestNumericArrays(object):
+
+# pylint: disable=no-self-use
+class TestNumericArrays:
+    """Test-suite for numeric-arrays behaviors."""
+
     def test_means_without_grouping_by_dimensions(self):
+        """Test means on no-dimensions measure of numeric array."""
         slice_ = Cube(NA.NUM_ARR_MEANS_NO_DIMS).partitions[0]
         np.testing.assert_equal(slice_.means, [2.5, 25])
+
+    def test_means_with_grouping_by_single_categorical_dimension(self):
+        """Test means on numeric array, grouped by single categorical dimension."""
+        slice_ = Cube(NA.MEANS_GROUPED_BY_SINGLE_CATEGORICAL).partitions[0]
+        np.testing.assert_equal(
+            slice_.means,
+            [
+                # subvars:
+                # 0001, 0002
+                [0, 0],  # cats: -- red
+                [1, 10],  # ------- green
+                [2, 20],  # ------- blue
+                [3, 30],  # ------- 4
+                [5, 50],  # ------- 9
+            ],
+        )
