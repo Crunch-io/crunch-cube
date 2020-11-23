42431dda6ec2b5f5a4e658775aada3f1d13a7d58
Slobodan Ilic <slobodan@crunch.io>
2020-11-23 18:12:43 +0100

    mtx: add regular factory methods
    * add _BaseCubeResultMatrix._get_sliced_counts()
    * add _BaseCubeResultMatrix._get_regular_matrix_factory_class()
    * add _BaseCubeResultMatrix._get_sliced_counts()
    * add _BaseCubeResultMatrix._get_regular_matrix_counts_slice()

--- src/cr/cube/matrix.py
+++ src/cr/cube/matrix.py
@@ -9,6 +9,9 @@ according to the dimension *order* transforms, and to hide rows and columns that
 either hidden by the user or "pruned" because they contain no observations.
 """
 
+import numpy as np
+
+from cr.cube.enums import DIMENSION_TYPE as DT
 from cr.cube.util import lazyproperty
 
 
@@ -97,6 +100,11 @@ class _SumSubtotals(_BaseSubtotals):
 class _BaseCubeResultMatrix(object):
     """Base class for all cube-result matrix (2D second-order analyzer) objects."""
 
+    def __init__(self, dimensions, counts, unweighted_counts):
+        self._dimensions = dimensions
+        self._counts = counts
+        self._unweighted_counts = unweighted_counts
+
     @classmethod
     def factory(cls, cube, dimensions, slice_idx):
         """Return a base-matrix object of appropriate type for `cube`."""
@@ -111,6 +119,50 @@ class _BaseCubeResultMatrix(object):
         return cls._regular_matrix_factory(cube, dimensions, slice_idx)
 
     @classmethod
+    def _get_sliced_counts(cls, cube, slice_idx):
+        """Returns a tuple of cube counts, prepared for regular matrix construction.
+
+        Depending on the type of the cube, we need to extract the proper counts for the
+        counstruction of a particular slice (matrix). In case of cubes that have more
+        then 2 dimensions, we only need a particular slice (a particular selected
+        element of the 0th dimension).
+
+        If, in addition to being >2D cube, the 0th dimension is multiple response, we
+        need to extract only the selected counts, since we're "just" dealing with the
+        tabulation.
+        """
+        i = cls._get_regular_matrix_counts_slice(cube, slice_idx)
+        return (cube.counts[i], cube.unweighted_counts[i], cube.counts_with_missings[i])
+
+    @staticmethod
+    def _get_regular_matrix_counts_slice(cube, slice_idx):
+        """return `np.s_` object with correct slicing for the cube type."""
+        if cube.ndim <= 2:
+            return np.s_[:]
+
+        # --- If 0th dimension of a >2D cube is MR, we only need the "Selected"
+        # --- counts, because it's "just" used to tabulate.
+        if cube.dimension_types[0] == DT.MR:
+            return np.s_[slice_idx, 0]
+
+        # --- If we have a cube with more than 2 dimensions we need to extract the
+        # --- appropriate slice (element of the 0th dimension).
+        return np.s_[slice_idx]
+
+    @staticmethod
+    def _get_regular_matrix_factory_class(dimension_types):
+        """Return correct class for matrix construction, based on dimension types."""
+        return (
+            _MrXMrMatrix
+            if dimension_types == (DT.MR, DT.MR)
+            else _MrXCatMatrix
+            if dimension_types[0] == DT.MR
+            else _CatXMrMatrix
+            if dimension_types[1] == DT.MR
+            else _CatXCatMatrix
+        )
+
+    @classmethod
     def _means_matrix_factory(cls, cube, dimensions, slice_idx):
         """ -> matrix object appropriate to means `cube`."""
         raise NotImplementedError
@@ -123,4 +175,117 @@ class _BaseCubeResultMatrix(object):
     @classmethod
     def _regular_matrix_factory(cls, cube, dimensions, slice_idx):
         """ -> matrix object for non-mr-aug and non-means slice."""
-        raise NotImplementedError
+        MatrixCls = cls._get_regular_matrix_factory_class(cube.dimension_types[-2:])
+        return MatrixCls(dimensions, *cls._get_sliced_counts(cube, slice_idx))
+
+
+class _CatXCatMatrix(_BaseCubeResultMatrix):
+    """Matrix for CAT_X_CAT cubes and base class for most other matrix classes.
+
+    Despite the name, this matrix is used for CA_SUBVAR and CA_CAT dimension too, since
+    these behave the same from a base-matrix perspective.
+
+    `counts_with_missings` is the raw weighted counts array, needed to compute the
+    column-index.
+    """
+
+    def __init__(
+        self, dimensions, counts, unweighted_counts, counts_with_missings=None
+    ):
+        super(_CatXCatMatrix, self).__init__(dimensions, counts, unweighted_counts)
+        self._counts_with_missings = counts_with_missings
+
+
+class _CatXMrMatrix(_CatXCatMatrix):
+    """Represents a CAT x MR slice.
+
+    Its `._counts` is a 3D ndarray with axes (rows, cols, selected/not), like:
+
+        [[[1002.52343241 1247.791605  ]
+          [ 765.95079804 1484.36423937]
+          [ 656.43937497 1593.87566244]]
+
+         [[1520.23482091 2573.22762247]
+          [1291.0925792  2802.36986418]
+          [1595.44412365 2498.01831973]]
+
+         [[ 908.65667501 2254.62623471]
+          [ 841.76439186 2321.51851785]
+          [1603.79596755 1559.48694217]]
+
+         [[ 746.89008236 1753.26322241]
+          [ 721.38248086 1778.7708239 ]
+          [1255.87038944 1244.28291533]]
+
+         [[   9.83166357   25.9551254 ]
+          [   8.23140253   27.55538645]
+          [  22.214956     13.57183298]]]
+
+    Each value is np.float64 if the cube-result is weighted (as in this example), or
+    np.int64 if unweighted.
+    """
+
+
+class _MrXCatMatrix(_CatXCatMatrix):
+    """Represents an MR_X_CAT slice.
+
+    Its `._counts` is a 3D ndarray with axes (rows, sel/not, cols), like:
+
+        [[[ 39  44  24  35]
+          [389 447 266 394]]
+
+         [[ 34  36  29  24]
+          [394 455 261 405]]
+
+         [[357 415 241 371]
+          [ 71  76  49  58]]
+
+         [[  0   0   0   0]
+          [428 491 290 429]]]
+
+    Each value is np.float64, or np.int64 if the cube-result is unweighted (as in this
+    example).
+    """
+
+
+class _MrXMrMatrix(_CatXCatMatrix):
+    """Represents an MR x MR slice.
+
+    Its `._counts` is a 4D ndarray with axes (rows, sel/not, cols, sel/not), like:
+
+        [[[[2990.03485848 4417.96127006]
+           [2713.94318797 4694.05294056]
+           [2847.96860219 4560.02752634]]
+
+          [[1198.10181578 3436.90253993]
+           [ 914.47846452 3720.52589119]
+           [2285.79620941 2349.2081463 ]]]
+
+
+         [[[2626.08325048 5180.55485426]
+           [2396.04310657 5410.59499817]
+           [3503.08635211 4303.55175262]]
+
+          [[1562.05342378 2674.30895573]
+           [1232.37854592 3003.98383359]
+           [1630.67845949 2605.68392002]]]
+
+
+         [[[3370.04923406 5278.54391705]
+           [3033.71862569 5614.87452542]
+           [3312.56140096 5336.03175016]]
+
+          [[ 818.0874402  2576.31989293]
+           [ 594.7030268  2799.70430633]
+           [1821.20341065 1573.20392249]]]
+
+
+         [[[1822.67560537 2883.99243344]
+           [1616.70492531 3089.96311351]
+           [1735.59793395 2971.07010487]]
+
+          [[2365.46106889 4970.87137654]
+           [2011.71672718 5324.61571825]
+           [3398.16687766 3938.16556777]]]]
+
+    """
--- tests/unit/test_matrix.py
+++ tests/unit/test_matrix.py
@@ -2,11 +2,20 @@
 
 """Unit test suite for `cr.cube.matrix` module."""
 
+import numpy as np
 import pytest
 
 from cr.cube.cube import Cube
 from cr.cube.dimension import Dimension
-from cr.cube.matrix import Assembler, _BaseCubeResultMatrix
+from cr.cube.enums import DIMENSION_TYPE as DT
+from cr.cube.matrix import (
+    Assembler,
+    _BaseCubeResultMatrix,
+    _CatXCatMatrix,
+    _CatXMrMatrix,
+    _MrXCatMatrix,
+    _MrXMrMatrix,
+)
 
 from ..unitutil import class_mock, instance_mock, method_mock, property_mock
 
@@ -108,6 +117,107 @@ class Describe_BaseCubeResultMatrix(object):
 
         factory_method.assert_called_once_with(cube_, (dimension_, dimension_), 71)
         assert cube_result_matrix is cube_result_matrix_
+
+    @pytest.mark.parametrize(
+        ("slice_idx", "dim_types", "expected"),
+        (
+            # <= 2D
+            (0, (DT.CAT,), np.s_[:]),
+            (1, (DT.CAT,), np.s_[:]),
+            (0, (DT.CAT, DT.CAT), np.s_[:]),
+            (1, (DT.CAT, DT.CAT), np.s_[:]),
+            # 3D, no MR as tabs
+            (0, (DT.CAT, DT.CAT, DT.CAT), np.s_[0]),
+            (1, (DT.CAT, DT.CAT, DT.CAT), np.s_[1]),
+            (2, (DT.CAT, DT.CAT, DT.CAT), np.s_[2]),
+            # 3D, MR as tabs
+            (0, (DT.MR, DT.CAT, DT.CAT), np.s_[0, 0]),
+            (1, (DT.MR, DT.CAT, DT.CAT), np.s_[1, 0]),
+            (2, (DT.MR, DT.CAT, DT.CAT), np.s_[2, 0]),
+        ),
+    )
+    def it_knows_its_regular_matrix_slice_for_counts_to_help(
+        self, cube_, slice_idx, dim_types, expected
+    ):
+        cube_.dimension_types = dim_types
+        cube_.ndim = len(dim_types)
+
+        s = _BaseCubeResultMatrix._get_regular_matrix_counts_slice(cube_, slice_idx)
+
+        assert s == expected
+
+    @pytest.mark.parametrize(
+        ("counts", "counts_slice", "expected"),
+        (
+            ([[1, 2, 3], [4, 5, 6]], np.s_[:], [[1, 2, 3], [4, 5, 6]]),
+            ([[1, 2, 3], [4, 5, 6]], np.s_[0], [1, 2, 3]),
+            ([[1, 2, 3], [4, 5, 6]], np.s_[0, 0], 1),
+        ),
+    )
+    def it_knows_how_to_get_sliced_counts_to_help(
+        self, request, cube_, counts, counts_slice, expected
+    ):
+        counts = np.array(counts)
+        cube_.counts = counts
+        cube_.unweighted_counts = counts
+        cube_.counts_with_missings = counts
+        fake_slice_idx = 1
+        _get_regular_matrix_counts_slice = method_mock(
+            request,
+            _BaseCubeResultMatrix,
+            "_get_regular_matrix_counts_slice",
+            return_value=counts_slice,
+        )
+
+        sliced_counts = _BaseCubeResultMatrix._get_sliced_counts(cube_, fake_slice_idx)
+
+        counts, unweighted, with_missing = sliced_counts
+        assert counts.tolist() == expected
+        assert unweighted.tolist() == expected
+        _get_regular_matrix_counts_slice.assert_called_once_with(cube_, fake_slice_idx)
+
+    @pytest.mark.parametrize(
+        ("dimension_types", "expected"),
+        (
+            ((DT.MR, DT.MR), _MrXMrMatrix),
+            ((DT.MR, DT.CAT), _MrXCatMatrix),
+            ((DT.CAT, DT.MR), _CatXMrMatrix),
+            ((DT.CAT, DT.CAT), _CatXCatMatrix),
+        ),
+    )
+    def it_knows_its_regular_matrix_factory_class(self, dimension_types, expected):
+        matrix_class = _BaseCubeResultMatrix._get_regular_matrix_factory_class(
+            dimension_types
+        )
+
+        assert matrix_class == expected
+
+    def it_creates_matrix_using_factory_to_help(self, request, cube_, dimension_):
+        cube_.dimension_types = (DT.CAT, DT.MR, DT.CAT)
+        MatrixCls_ = class_mock(request, "cr.cube.matrix._BaseCubeResultMatrix")
+        _get_regular_matrix_factory_class = method_mock(
+            request,
+            _BaseCubeResultMatrix,
+            "_get_regular_matrix_factory_class",
+            return_value=MatrixCls_,
+        )
+        _get_sliced_counts = method_mock(
+            request,
+            _BaseCubeResultMatrix,
+            "_get_sliced_counts",
+            return_value=([[1], [2]], [[3], [4]]),
+        )
+
+        matrix = _BaseCubeResultMatrix._regular_matrix_factory(
+            cube_, (dimension_, dimension_), slice_idx=17
+        )
+
+        _get_regular_matrix_factory_class.assert_called_once_with((DT.MR, DT.CAT))
+        _get_sliced_counts.assert_called_once_with(cube_, 17)
+        MatrixCls_.assert_called_once_with(
+            (dimension_, dimension_), [[1], [2]], [[3], [4]]
+        )
+        assert matrix is MatrixCls_.return_value
 
     # fixture components ---------------------------------------------
 
