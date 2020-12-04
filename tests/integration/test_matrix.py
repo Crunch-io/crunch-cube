d162b770cb07f5f71d141573356981ba86612eb9
Steve Canny <stcanny@gmail.com>
2020-12-03 19:12:35 -0800

    xfail: add remaining ucount XFAILs

--- tests/integration/test_matrix.py
+++ tests/integration/test_matrix.py
@@ -3,6 +3,7 @@
 """Integration-test suite for `cr.cube.cubepart` module."""
 
 import numpy as np
+import pytest
 
 from cr.cube.cube import Cube
 from cr.cube.cubepart import _Slice
@@ -39,4 +40,132 @@ class DescribeAssembler(object):
                 [100, 49, 51, 23, 23],
                 [341, 175, 166, 234, 234],
             ],
+        )
+
+    @pytest.mark.xfail(reason="WIP", raises=NotImplementedError, strict=True)
+    def it_computes_assembled_ucounts_for_ca_subvar_x_ca_cat_hiddens(self):
+        """Assembler hides, prunes, and places in payload order.
+
+        This fixture has no insertions, and exercises the "no-insertions" case which
+        requires certain subtle special handling.
+        """
+        slice_ = _Slice(
+            Cube(CR.CA_SUBVAR_X_CA_CAT_EMPTIES),
+            slice_idx=0,
+            transforms={
+                "rows_dimension": {"elements": {"5": {"hide": True}}, "prune": True},
+                "columns_dimension": {
+                    "elements": {"99": {"hide": True}},
+                    "prune": True,
+                },
+            },
+            population=None,
+            mask_size=0,
+        )
+
+        unweighted_counts = slice_._assembler.unweighted_counts
+
+        assert np.array_equal(
+            unweighted_counts,
+            [
+                [2734, 5887, 1017],
+                [2810, 7000, 474],
+                [347, 2577, 4467],
+            ],
+        )
+
+    def it_computes_assembled_ucounts_for_cat_hs_x_cat_hs_hiddens_explicit_order(self):
+        """Assembler inserts, hides, prunes, and places in explicit order."""
+        slice_ = _Slice(
+            Cube(CR.CAT_HS_X_CAT_HS_EMPTIES),
+            slice_idx=0,
+            transforms={
+                "rows_dimension": {
+                    "elements": {"2": {"hide": True}},
+                    "prune": True,
+                    "order": {"type": "explicit", "element_ids": [0, 5, 2, 1, 4]},
+                },
+                "columns_dimension": {
+                    "elements": {"2": {"hide": True}},
+                    "prune": True,
+                    "order": {"type": "explicit", "element_ids": [4, 2, 5, 0]},
+                },
+            },
+            population=None,
+            mask_size=0,
+        )
+
+        unweighted_counts = slice_._assembler.unweighted_counts
+
+        assert np.array_equal(
+            unweighted_counts,
+            [
+                [118, 33, 53, 65, 33],
+                [163, 190, 70, 93, 190],
+                [331, 272, 172, 159, 272],
+                [40, 12, 8, 32, 12],
+                [168, 82, 102, 66, 82],
+                [100, 23, 51, 49, 23],
+                [341, 234, 166, 175, 234],
+            ],
+        )
+
+    @pytest.mark.xfail(reason="WIP", raises=ValueError, strict=True)
+    def it_computes_assembled_ucounts_for_cat_hs_x_mr_hiddens_explicit_order(self):
+        """Assembler inserts, hides, prunes, and places in explicit order."""
+        slice_ = _Slice(
+            Cube(CR.CAT_HS_X_MR),
+            slice_idx=0,
+            transforms={
+                "rows_dimension": {
+                    "elements": {"5": {"hide": True}},
+                    "prune": True,
+                    "order": {"type": "explicit", "element_ids": [6, 1, 5, 2, 4, 0]},
+                },
+                "columns_dimension": {
+                    "elements": {"1": {"hide": True}},
+                    "order": {"type": "explicit", "element_ids": [5, 1, 4, 2, 3]},
+                },
+            },
+            population=None,
+            mask_size=0,
+        )
+
+        unweighted_counts = slice_._assembler.unweighted_counts
+
+        assert np.array_equal(
+            unweighted_counts,
+            [
+                [300, 320, 53, 93],
+                [27, 13, 7, 4],
+                [58, 36, 16, 21],
+                [85, 49, 23, 25],
+                [134, 130, 26, 39],
+            ],
+        )
+
+    @pytest.mark.xfail(reason="WIP", raises=NotImplementedError, strict=True)
+    def it_computes_assembled_ucounts_for_mr_x_cat(self):
+        slice_ = Cube(CR.MR_X_CAT).partitions[0]
+        assert np.array_equal(
+            slice_._assembler.unweighted_counts,
+            [
+                [8, 7, 0, 6, 5, 0],
+                [7, 16, 0, 26, 27, 0],
+                [4, 21, 0, 39, 54, 0],
+                [13, 36, 0, 130, 190, 0],
+                [27, 58, 0, 134, 166, 0],
+            ],
+        )
+
+    @pytest.mark.xfail(reason="WIP", raises=NotImplementedError, strict=True)
+    def it_computes_assembled_ucounts_for_mr_x_mr_slices(self):
+        slice_ = Cube(CR.CAT_X_MR_X_MR).partitions[0]
+        np.testing.assert_array_equal(
+            slice_._assembler.unweighted_counts, [[1159, 3597], [197, 604], [192, 582]]
+        )
+
+        slice_ = Cube(CR.CAT_X_MR_X_MR).partitions[1]
+        np.testing.assert_array_equal(
+            slice_._assembler.unweighted_counts, [[159, 94], [1182, 625], [1142, 623]]
         )
