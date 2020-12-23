50de2d412d358d625ea5179bd3f3598549129077
ernestoarbitrio <ernesto.arbitrio@gmail.com>
2020-12-23 15:55:01 +0100

    release: prepare v1.12.11b release

--- src/cr/cube/__init__.py
+++ src/cr/cube/__init__.py
@@ -2,6 +2,6 @@
 
 """Initialization module for crunch-cube package."""
 
-__version__ = "1.12.10"
+__version__ = "1.12.11b"
 
 # NOTE: We'll be switching to 2.0.0 once we throw out the old cube and slice
