a106af906650aca7cefa0b623e75b637deac543a
Ernesto Arbitrio <ernesto.arbitrio@gmail.com>
2021-01-18 14:12:03 +0100

    back to 100% coverage

--- tests/integration/test_cube.py
+++ tests/integration/test_cube.py
@@ -109,10 +109,20 @@ class DescribeIntegratedCube(object):
 
         np.testing.assert_array_equal(cube.valid_counts, [[3, 3, 1], [2, 1, 1]])
 
+    def and_it_returns_empty_array_if_valid_counts_are_not_available(self):
+        cube = Cube(CR.CAT_X_CAT)
+
+        np.testing.assert_array_equal(cube.valid_counts, [])
+
     def it_provides_valid_counts_summary_for_NUM_ARRAY_GROUPED_BY_CAT(self):
         cube = Cube(NA.NUM_ARR_MEANS_GROUPED_BY_CAT)
 
         np.testing.assert_array_equal(cube.valid_counts_summary, [5, 4, 2])
+
+    def and_it_returns_empty_array_for_summary_if_valid_counts_are_not_available(self):
+        cube = Cube(CR.CAT_X_CAT)
+
+        np.testing.assert_array_equal(cube.valid_counts_summary, [])
 
     def it_provides_n_responses_for_NUM_ARRAY_GROUPED_BY_CAT(self):
         cube = Cube(NA.NUM_ARR_MEANS_GROUPED_BY_CAT)
