2ecb2dd85e5b1e13c1cb476df1d19fe1493b3d95
Steve Canny <stcanny@gmail.com>
2021-02-11 10:29:22 -0800

    xfail: add XFAIL for StripeAssembler.unweighted_counts

---
+++ src/cr/cube/stripe/assembler.py
@@ -0,0 +1,39 @@
+# encoding: utf-8
+
+"""The `StripeAssembler` object provides the external interface for this module.
+
+Its name derives from its role to "assemble" a finished 1D array ("stripe") for a
+particular measure from the base measure values and inserted subtotals, to reorder the
+rows according to the dimension *order* transforms, and to hide rows that are either
+hidden by the user or "pruned" because they contain no observations.
+"""
+
+from __future__ import division
+
+from cr.cube.util import lazyproperty
+
+
+class StripeAssembler(object):
+    """Provides measures, marginals, and totals for a (1D) strand cube-slice.
+
+    An assembled stripe measure is a 1D ndarray reflecting all ordering, insertion, and
+    hiding transforms applied to the dimension. An assembled margin is often a scalar.
+
+    `cube` is the `cr.cube.Cube` object containing the data for this matrix.
+
+    `rows_dimension` is the Dimension object describing the stripe.
+
+    `slice_idx` is an int offset indicating which portion of `cube` data to use for this
+    stripe.
+    """
+
+    def __init__(self, cube, rows_dimension, ca_as_0th, slice_idx):
+        self._cube = cube
+        self._rows_dimension = rows_dimension
+        self._ca_as_0th = ca_as_0th
+        self._slice_idx = slice_idx
+
+    @lazyproperty
+    def unweighted_counts(self):
+        """1D np.int64 ndarray of unweighted count for each row of stripe."""
+        raise NotImplementedError
--- tests/integration/test_stripe.py
+++ tests/integration/test_stripe.py
@@ -1,3 +1,38 @@
 # encoding: utf-8
 
 """Integration-test suite for `cr.cube.stripe` sub-package."""
+
+import pytest
+
+from cr.cube.cube import Cube
+from cr.cube.stripe.assembler import StripeAssembler
+
+from ..fixtures import CR
+
+
+class DescribeStripeAssembler(object):
+    """Integration-test suite for `cr.cube.stripe.assembler.StripeAssembler` object."""
+
+    @pytest.mark.xfail(reason="WIP", raises=NotImplementedError, strict=True)
+    def it_provides_values_for_univariate_cat(self):
+        cube = Cube(CR.UNIVARIATE_CATEGORICAL)
+        assembler = StripeAssembler(cube, cube.dimensions[0], False, 0)
+
+        assert assembler.unweighted_counts.tolist() == [10, 5]
+
+    @pytest.mark.xfail(reason="WIP", raises=NotImplementedError, strict=True)
+    def it_provides_values_for_univariate_mr(self):
+        cube = Cube(CR.MR_WGTD)
+        assembler = StripeAssembler(cube, cube.dimensions[0], False, 0)
+
+        assert assembler.unweighted_counts.tolist() == [
+            21545,
+            9256,
+            13412,
+            8562,
+            27380,
+            5041,
+            676,
+            1281,
+            3112,
+        ]
