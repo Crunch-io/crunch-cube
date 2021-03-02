b214681eaaaeca8d0322deac702d7550357f38f0
Greg Freedman Ellis <greg@crunch.io>
2021-03-02 14:51:00 -0600

    fix test broken during rebase

--- tests/unit/stripe/test_measure.py
+++ tests/unit/stripe/test_measure.py
@@ -717,13 +717,13 @@ class Describe_WeightedCounts(object):
         property_mock(
             request, _WeightedCounts, "base_values", return_value=[1.1, 2.2, 3.3]
         )
-        SumSubtotals_ = class_mock(request, "cr.cube.stripe.measure.SumSubtotals")
-        SumSubtotals_.subtotal_values.return_value = np.array([3.3, 5.5])
+        SumDiffSubtotals_ = class_mock(request, "cr.cube.stripe.measure.SumDiffSubtotals")
+        SumDiffSubtotals_.subtotal_values.return_value = np.array([3.3, 5.5])
         weighted_counts = _WeightedCounts(rows_dimension_, None, None)
 
         subtotal_values = weighted_counts.subtotal_values
 
-        SumSubtotals_.subtotal_values.assert_called_once_with(
+        SumDiffSubtotals_.subtotal_values.assert_called_once_with(
             [1.1, 2.2, 3.3], rows_dimension_
         )
         assert subtotal_values.tolist() == [3.3, 5.5]
