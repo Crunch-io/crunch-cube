c3f2503e561c8e7016b9a9c13713fbb3b2941a8d
Arbitrio <ernesto.arbitrio@gmail.com>
2021-03-16 15:33:16 +0100

    refactor numeric array integration tests with pytest.approx notation

--- tests/integration/test_numeric_array.py
+++ tests/integration/test_numeric_array.py
@@ -16,83 +16,86 @@ class DescribeNumericArrays(object):
     def it_provides_means_scale_measures(self):
         slice_ = Cube(NA.NUM_ARR_MEANS_SCALE_MEASURES).partitions[0]
 
-        np.testing.assert_array_almost_equal(
-            slice_.means,
-            [
-                # ------------- CAT -----------------
-                # C1      C2    C3       C4      C5
-                [np.nan, 70.00000, 53.2000, 62.752044, 70.0000],  # S1 (num array)
-                [np.nan, 66.666667, 70.0, 65.585284, 67.692308],  # S2 (num array)
-                [np.nan, 65.00000, 63.400, 62.38806, 49.230769],  # S3 (num array)
-                [np.nan, 63.333333, 26.8, 57.029973, 39.230769],  # S4 (num array)
-            ],
+        assert slice_.means == pytest.approx(
+            np.array(
+                [
+                    # ------------- CAT -----------------
+                    # C1      C2    C3       C4      C5
+                    [np.nan, 70.00000, 53.2000, 62.752044, 70.0000],  # S1 (num array)
+                    [np.nan, 66.666667, 70.0, 65.585284, 67.692308],  # S2 (num array)
+                    [np.nan, 65.00000, 63.400, 62.38806, 49.230769],  # S3 (num array)
+                    [np.nan, 63.333333, 26.8, 57.029973, 39.230769],  # S4 (num array)
+                ],
+            ),
+            nan_ok=True,
         )
-        np.testing.assert_array_almost_equal(
-            slice_.rows_scale_median, [3.0, 3.0, 3.0, 3.0]
-        )
+        assert slice_.rows_scale_median == pytest.approx([3.0, 3.0, 3.0, 3.0])
 
     def it_provides_means_for_num_array_grouped_by_cat(self):
         """Test means on numeric array, grouped by single categorical dimension."""
         slice_ = Cube(NA.NUM_ARR_MEANS_GROUPED_BY_CAT).partitions[0]
 
-        np.testing.assert_almost_equal(
-            slice_.means,
-            [  # -------Gender-----
-                #     M        F
-                [87.6666667, 52.5],  # S1: Dark Night
-                [93.3333333, 50.0],  # S2: Fight Club
-                [1.00000000, 45.0],  # S3: Meet the parents
-            ],
+        assert slice_.means == pytest.approx(
+            np.array(
+                [  # -------Gender-----
+                    #     M        F
+                    [87.6666667, 52.5],  # S1: Dark Night
+                    [93.3333333, 50.0],  # S2: Fight Club
+                    [1.00000000, 45.0],  # S3: Meet the parents
+                ],
+            )
         )
-        np.testing.assert_almost_equal(slice_.columns_base, [[3, 2], [3, 1], [1, 1]])
+        assert slice_.columns_base == pytest.approx(np.array([[3, 2], [3, 1], [1, 1]]))
 
     def it_provides_means_for_num_array_grouped_by_date(self):
         """Test means on numeric array, grouped by single categorical dimension."""
         slice_ = Cube(NA.NUM_ARR_MEANS_GROUPED_BY_DATE).partitions[0]
 
-        np.testing.assert_almost_equal(
-            slice_.means,
-            [  # -------Wave-----
-                # 2014-12   2015-01
-                [50.000000, 100.0000],  # S1: Mov A
-                [90.000000, 81.00000],  # S2: Mov B
-                [46.6666667, 80.0000],  # S3: Mov C
-            ],
+        assert slice_.means == pytest.approx(
+            np.array(
+                [  # -------Wave-----
+                    # 2014-12   2015-01
+                    [50.000000, 100.0000],  # S1: Mov A
+                    [90.000000, 81.00000],  # S2: Mov B
+                    [46.6666667, 80.0000],  # S3: Mov C
+                ],
+            )
         )
-        np.testing.assert_almost_equal(slice_.columns_base, [[10, 9], [8, 10], [9, 10]])
+        assert slice_.columns_base == pytest.approx(
+            np.array([[10, 9], [8, 10], [9, 10]])
+        )
 
     def it_provides_means_for_num_array_grouped_by_cat_weighted(self):
         """Test means on numeric array, grouped by single categorical dimension."""
         slice_ = Cube(NA.NUM_ARR_MEANS_GROUPED_BY_CAT_WEIGHTED).partitions[0]
 
-        np.testing.assert_array_almost_equal(
-            slice_.means,
-            [
-                #  Gender:
-                #  M  F
-                [91.62499999, 50.71428571],  # S1 (num array)
-                [94.37500000, 50.00000],  # S2 (num array)
-                [1.000000000, 45.00000],  # S3 (num array)
-            ],
+        assert slice_.means == pytest.approx(
+            np.array(
+                [
+                    #  Gender:
+                    #  M  F
+                    [91.62499999, 50.71428571],  # S1 (num array)
+                    [94.37500000, 50.00000],  # S2 (num array)
+                    [1.000000000, 45.00000],  # S3 (num array)
+                ],
+            )
         )
-        np.testing.assert_almost_equal(slice_.columns_base, [[3, 2], [3, 1], [1, 1]])
+        assert slice_.columns_base == pytest.approx(np.array([[3, 2], [3, 1], [1, 1]]))
 
     def it_provides_means_for_num_array_x_mr(self):
         slice_ = Cube(NA.NUM_ARR_MEANS_X_MR).partitions[0]
+        expected_means = [
+            # -------------------------MR--------------------------
+            #     S1         S2         S3         S4        S5
+            [4.5526316, 4.7857143, 4.3333333, 4.4444444, 4.5526316],  # S1 (num arr)
+            [3.7105263, 3.8571429, 3.8333333, 3.5555556, 3.7105263],  # S2 (num arr)
+        ]
 
-        np.testing.assert_almost_equal(
-            slice_.means,
-            [
-                # -------------------------MR--------------------------
-                #     S1         S2         S3         S4        S5
-                [4.5526316, 4.7857143, 4.3333333, 4.4444444, 4.5526316],  # S1 (num arr)
-                [3.7105263, 3.8571429, 3.8333333, 3.5555556, 3.7105263],  # S2 (num arr)
-            ],
-        )
+        assert slice_.means == pytest.approx(np.array(expected_means))
         # ---The columns_base is 2D because a NUM_ARR_X_MR matrix has a distinct
         # ---column-base for each cell.
-        np.testing.assert_almost_equal(
-            slice_.columns_base, [[38, 14, 6, 18, 38], [38, 14, 6, 18, 38]]
+        assert slice_.columns_base == pytest.approx(
+            np.array([[38, 14, 6, 18, 38], [38, 14, 6, 18, 38]])
         )
 
     def it_provides_means_for_numeric_array_with_no_grouping(self):
@@ -108,38 +111,41 @@ class DescribeNumericArrays(object):
         """Test sum on numeric array, grouped by single categorical dimension."""
         slice_ = Cube(NA.NUM_ARR_SUM_GROUPED_BY_CAT).partitions[0]
 
-        np.testing.assert_almost_equal(
-            slice_.sums,
-            [
-                #  --------Gender------------
-                # M    F
-                [4.0, 3.0],  # S1 (Ticket Sold)
-                [3.0, 0.0],  # S2 (Ticket Sold)
-                [2.0, 3.0],  # S3 (Ticket Sold)
-            ],
+        assert slice_.sums == pytest.approx(
+            np.array(
+                [
+                    #  --------Gender------------
+                    # M    F
+                    [4.0, 3.0],  # S1 (Ticket Sold)
+                    [3.0, 0.0],  # S2 (Ticket Sold)
+                    [2.0, 3.0],  # S3 (Ticket Sold)
+                ],
+            )
         )
-        np.testing.assert_almost_equal(slice_.columns_base, [[3, 2], [3, 2], [3, 2]])
+        assert slice_.columns_base == pytest.approx(np.array([[3, 2], [3, 2], [3, 2]]))
 
     def it_provides_sum_for_num_array_grouped_by_cat_hs(self):
         slice_ = Cube(NA.NUM_ARR_SUM_GROUPED_BY_CAT_HS).partitions[0]
 
-        np.testing.assert_almost_equal(
-            slice_.sums,
-            [
-                #  --------Gender------------
-                # M    F   M+F
-                [4.0, 3.0, 7.0],  # S1 (Ticket Sold)
-                [3.0, 0.0, 3.0],  # S2 (Ticket Sold)
-                [2.0, 3.0, 5.0],  # S3 (Ticket Sold)
-            ],
+        assert slice_.sums == pytest.approx(
+            np.array(
+                [
+                    #  --------Gender------------
+                    # M    F   M+F
+                    [4.0, 3.0, 7.0],  # S1 (Ticket Sold)
+                    [3.0, 0.0, 3.0],  # S2 (Ticket Sold)
+                    [2.0, 3.0, 5.0],  # S3 (Ticket Sold)
+                ],
+            )
         )
-        np.testing.assert_almost_equal(
-            slice_.columns_base,
-            [
-                [3, 2, 5],
-                [3, 2, 5],
-                [3, 2, 5],
-            ],
+        assert slice_.columns_base == pytest.approx(
+            np.array(
+                [
+                    [3, 2, 5],
+                    [3, 2, 5],
+                    [3, 2, 5],
+                ],
+            )
         )
 
     def it_provides_stddev_for_numeric_array_with_no_grouping(self):
@@ -155,31 +161,35 @@ class DescribeNumericArrays(object):
         """Test stddev on numeric array, grouped by single categorical dimension."""
         slice_ = Cube(NA.NUM_ARR_STDDEV_GROUPED_BY_CAT).partitions[0]
 
-        np.testing.assert_almost_equal(
-            slice_.stddev,
-            [
-                #  --------Gender------------
-                #     M         F
-                [8.7368949, 17.6776695],  # S1 (Ticket Sold)
-                [2.8867513, np.nan],  # S2 (Ticket Sold)
-                [np.nan, np.nan],  # S3 (Ticket Sold)
-            ],
+        assert slice_.stddev == pytest.approx(
+            np.array(
+                [
+                    #  --------Gender------------
+                    #     M         F
+                    [8.7368949, 17.6776695],  # S1 (Ticket Sold)
+                    [2.8867513, np.nan],  # S2 (Ticket Sold)
+                    [np.nan, np.nan],  # S3 (Ticket Sold)
+                ],
+            ),
+            nan_ok=True,
         )
-        np.testing.assert_almost_equal(slice_.columns_base, [[3, 2], [3, 1], [1, 1]])
+        assert slice_.columns_base == pytest.approx(np.array([[3, 2], [3, 1], [1, 1]]))
 
     def it_provides_stddev_for_num_array_x_mr(self):
         slice_ = Cube(NA.NUM_ARR_STDDEV_X_MR).partitions[0]
 
-        np.testing.assert_almost_equal(
-            slice_.stddev,
-            [
-                # -------------------------MR----------------
-                #     S1      S2       S3
-                [1.4142136, np.nan, np.nan],  # S1 (num arr)
-                [np.nan, np.nan, np.nan],  # S2 (num arr)
-                [np.nan, np.nan, np.nan],  # S3 (num arr)
-            ],
+        assert slice_.stddev == pytest.approx(
+            np.array(
+                [
+                    # -------------------------MR----------------
+                    #     S1      S2       S3
+                    [1.4142136, np.nan, np.nan],  # S1 (num arr)
+                    [np.nan, np.nan, np.nan],  # S2 (num arr)
+                    [np.nan, np.nan, np.nan],  # S3 (num arr)
+                ],
+            ),
+            nan_ok=True,
         )
-        np.testing.assert_almost_equal(
-            slice_.columns_base, [[2, 1, 1], [1, 0, 0], [1, 1, 1]]
+        assert slice_.columns_base == pytest.approx(
+            np.array([[2, 1, 1], [1, 0, 0], [1, 1, 1]])
         )
