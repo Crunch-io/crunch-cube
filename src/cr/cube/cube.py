acde9d3c9957bad7409f8c64825347218cdc567f
Slobodan Ilic <slobodan@crunch.io>
2021-04-27 12:11:30 +0200

    [#177923295]: Ernesto's comments
    * Rename oddly named variable

--- src/cr/cube/cube.py
+++ src/cr/cube/cube.py
@@ -734,16 +734,18 @@ class _Measures(object):
 
         # Try and get the new-style complete-cases filtered counts
         filter_stats = self._cube_dict["result"].get("filter_stats", {})
-        weighted = filter_stats.get("filtered_complete", {}).get("weighted")
-        if weighted:
+        weighted_filtered_complete = filter_stats.get("filtered_complete", {}).get(
+            "weighted"
+        )
+        if weighted_filtered_complete:
             # ---If the filter consists of a single categorical date variable, we need
             # ---to treat the population fraction as 1, as it's constant accross dates
             if filter_stats.get("is_cat_date"):
                 return 1
 
             # ---If new format is present in response json, use that for pop fraction
-            numerator = weighted["selected"]
-            denominator = numerator + weighted["other"]
+            numerator = weighted_filtered_complete["selected"]
+            denominator = numerator + weighted_filtered_complete["other"]
         else:
             # ---If new format is not available, default to old-style calculation
             numerator = self._cube_dict["result"].get("filtered", {}).get("weighted_n")
