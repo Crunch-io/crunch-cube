[178528445]: fix for ttest base mistake and new formula

src/cr/cube/matrix/measure.py
@@ -953,23 +953,25 @@ class _PairwiseMeansSigTStats(_BaseSecondOrderMeasure):
     @lazyproperty
     def t_stats(self):
         """2D ndarray of float64 representing t-stats for means pairwise testing.
-
         Calculate the level of significance for the difference of two means from the
         selected column different from a hypothesized value.
-        t = (x̄1 - x̄2 - μ) √ (N / s2)
-        s2 = var1 + var2 - 2 * covar12 - covar12 in this specific case is 0.
+        t = (x̄1 - x̄2 - (μ1 - μ2)) / √ ((s1 / N1) + (s2 / N2))
         """
         if self._selected_column_idx < 0:
             return np.full(self._cube_measures.cube_stddev.stddev.shape, np.nan)
 
         means = self._cube_measures.cube_means.means
         variance = np.power(self._cube_measures.cube_stddev.stddev, 2)
-        col_bases = self._cube_measures.old_unwted_cube_counts.column_bases
+        col_bases = self._cube_measures.unweighted_cube_counts.column_bases
+        idx = self._selected_column_idx
 
-        combined_variance = variance[:, self._selected_column_idx] + variance.T
-        diff = means.T - means[:, self._selected_column_idx]
-        n = col_bases[:, self._selected_column_idx] + col_bases.T
-        return diff.T * np.sqrt(n.T / combined_variance.T)
+        ref_means = np.broadcast_to(means[:, idx][:, None], means.shape)
+        ref_variance = np.broadcast_to(variance[:, idx][:, None], variance.shape)
+        ref_col_bases = np.broadcast_to(col_bases[:, idx][:, None], col_bases.shape)
+
+        return (means - ref_means) / np.sqrt(
+            (variance / col_bases) + (ref_variance / ref_col_bases)
+        )
 
 
 class _PairwiseMeansSigPVals(_PairwiseMeansSigTStats):

tests/integration/test_pairwise_significance.py
@@ -981,24 +981,25 @@ class TestMeanDifferenceSignificance(object):
         assert slice_.pairwise_significance_means_t_stats(0) == pytest.approx(
             np.array(
                 [
-                    [-0.0, -0.7482866, -4.61524529, -5.29463547],
-                    [-0.0, -4.64371041, -7.14738681, -3.67511747],
-                    [-0.0, 3.13387218, 6.35310465, 2.3932706],
+                    [0.0, -0.32190273, -1.884166, -2.16152588],
+                    [0.0, -1.91311986, -2.91790845, -1.50036042],
+                    [0.0, 1.18770459, 2.59364411, 0.97704863],
                 ]
             )
         )
         assert slice_.pairwise_significance_means_p_vals(0) == pytest.approx(
             np.array(
                 [
-                    [1.00000000e00, 4.60526868e-01, 5.38716457e-05, 7.11866628e-06],
-                    [1.00000000e00, 7.34317022e-05, 2.90876736e-08, 8.12768987e-04],
-                    [1.00000000e00, 4.02303806e-03, 3.00760739e-07, 2.23647892e-02],
+                    [1.0, 0.74991738, 0.06811651, 0.03778574],
+                    [1.0, 0.06600917, 0.00620631, 0.14275097],
+                    [1.0, 0.24492773, 0.01390786, 0.33544613],
                 ]
             )
         )
 
     def test_mean_diff_significance_for_numeric_array_grouped_by_cat_hs(self):
         transforms = {
+            "pairwise_indices": {"alpha": [0.5]},
             "columns_dimension": {
                 "insertions": [
                     {
@@ -1028,7 +1029,7 @@ class TestMeanDifferenceSignificance(object):
                         "kwargs": {"negative": [4, 5]},
                     },
                 ]
-            }
+            },
         }
         slice_ = Cube(
             NA.NUM_ARR_MEANS_GROUPED_BY_CAT_HS, transforms=transforms
@@ -1042,13 +1043,17 @@ class TestMeanDifferenceSignificance(object):
             ).tolist() == pytest.approx(
                 np.full(slice_.means.shape, np.nan), nan_ok=True
             )
-        np.testing.assert_almost_equal(
-            slice_.pairwise_significance_means_t_stats(7),
-            load_python_expression("num-arr-means-grouped-by-cat-hs-t-stats-col-7"),
+        assert slice_.pairwise_significance_means_t_stats(7) == pytest.approx(
+            np.array(
+                load_python_expression("num-arr-means-grouped-by-cat-hs-t-stats-col-7")
+            ),
+            nan_ok=True,
         )
-        np.testing.assert_almost_equal(
-            slice_.pairwise_significance_means_p_vals(7),
-            load_python_expression("num-arr-means-grouped-by-cat-hs-p-vals-col-7"),
+        assert slice_.pairwise_significance_means_p_vals(7) == pytest.approx(
+            np.array(
+                load_python_expression("num-arr-means-grouped-by-cat-hs-p-vals-col-7")
+            ),
+            nan_ok=True,
         )
         assert slice_.pairwise_means_indices.tolist() == [
             [(), (), (7,), (), (), (), (), (), ()],
@@ -1058,20 +1063,27 @@ class TestMeanDifferenceSignificance(object):
         ]
 
         # Test no subtotals
-        slice_ = Cube(NA.NUM_ARR_MEANS_GROUPED_BY_CAT_HS).partitions[0]
+        transforms = {"pairwise_indices": {"alpha": [0.5]}}
+        slice_ = Cube(
+            NA.NUM_ARR_MEANS_GROUPED_BY_CAT_HS, transforms=transforms
+        ).partitions[0]
         assert slice_.pairwise_means_indices.tolist() == [
             [(), (4,), (), (), ()],
             [(), (), (), (), ()],
             [(), (0, 2, 4), (), (0, 2, 4), ()],
             [(), (), (), (), ()],
         ]
-        np.testing.assert_almost_equal(
-            slice_.pairwise_significance_means_t_stats(4),
-            load_python_expression("num-arr-means-grouped-by-cat-t-stats-col-4"),
+        assert slice_.pairwise_significance_means_t_stats(4) == pytest.approx(
+            np.array(
+                load_python_expression("num-arr-means-grouped-by-cat-t-stats-col-4")
+            ),
+            nan_ok=True,
         )
-        np.testing.assert_almost_equal(
-            slice_.pairwise_significance_means_p_vals(4),
-            load_python_expression("num-arr-means-grouped-by-cat-p-vals-col-4"),
+        assert slice_.pairwise_significance_means_p_vals(4) == pytest.approx(
+            np.array(
+                load_python_expression("num-arr-means-grouped-by-cat-p-vals-col-4")
+            ),
+            nan_ok=True,
         )
 
     def test_mean_diff_significance_for_numeric_array_x_mr(self):
@@ -1080,10 +1092,10 @@ class TestMeanDifferenceSignificance(object):
         assert slice_.pairwise_significance_means_t_stats(0) == pytest.approx(
             np.array(
                 [
-                    [-0.0, 1.3056921, -1.1842639],
-                    [-0.0, 1.7117695, -1.6297401],
+                    [0, 0.461036, -0.41818914],
+                    [0, 0.64326512, -0.5674027],
                     [np.nan, np.nan, np.nan],
-                    [np.nan, -5.8872721, -4.4442801],
+                    [np.nan, -2.29161759, -1.72993372],
                 ]
             ),
             nan_ok=True,
@@ -1091,17 +1103,17 @@ class TestMeanDifferenceSignificance(object):
         assert slice_.pairwise_significance_means_p_vals(0) == pytest.approx(
             np.array(
                 [
-                    [1.00000000e00, 1.96331347e-01, 2.40688158e-01],
-                    [1.00000000e00, 9.17815019e-02, 1.08068956e-01],
+                    [1, 0.64633575, 0.67720874],
+                    [1, 0.52235034, 0.57242562],
                     [np.nan, np.nan, np.nan],
-                    [np.nan, 1.57810759e-07, 3.57756031e-05],
+                    [np.nan, 0.02522834, 0.08846228],
                 ]
             ),
             nan_ok=True,
         )
 
     def test_mean_diff_significance_indices_for_numeric_array_grouped_by_cat(self):
-        transforms = {"pairwise_indices": {"alpha": [0.05, 0.08]}}
+        transforms = {"pairwise_indices": {"alpha": [0.45, 0.40]}}
         slice_ = Cube(
             NA.NUM_ARR_MULTI_NUMERIC_MEASURES_GROUPED_BY_CAT, transforms=transforms
         ).partitions[0]
@@ -1156,8 +1168,8 @@ class TestMeanDifferenceSignificance(object):
             np.array(
                 [
                     [np.nan, np.nan, np.nan, np.nan],
-                    [14.1421356, -0.0, 3.1066887, np.nan],
-                    [1.75662013, -0.0, np.nan, np.nan],
+                    [9.35414347, 0.0, 2.1529364, np.nan],
+                    [1.26202085, 0, np.nan, np.nan],
                 ]
             ),
             nan_ok=True,
@@ -1166,14 +1178,14 @@ class TestMeanDifferenceSignificance(object):
             np.array(
                 [
                     [np.nan, np.nan, np.nan, np.nan],
-                    [8.21565038e-15, 1.00000000e00, 4.80968355e-03, np.nan],
-                    [8.91869315e-02, 1.00000000e00, np.nan, np.nan],
+                    [2.11584750e-10, 1.00000000e00, 4.15883131e-02, np.nan],
+                    [2.16664992e-01, 1.00000000e00, np.nan, np.nan],
                 ]
             ),
             nan_ok=True,
         )
         assert slice_.pairwise_means_indices.tolist() == [
-            [(), (), (), (0, 2)],
+            [(), (), (), ()],
             [(1, 2), (), (1,), ()],
             [(), (), (), ()],
         ]
@@ -1183,7 +1195,7 @@ class TestMeanDifferenceSignificance(object):
             CR.MEAN_CAT_X_CAT,
             transforms={
                 "rows_dimension": {
-                    "elements": {"2": {"hide": True}},
+                    "elements": {"1": {"hide": True}},
                     "prune": True,
                 },
                 "columns_dimension": {
@@ -1195,13 +1207,13 @@ class TestMeanDifferenceSignificance(object):
         ).partitions[0]
 
         assert slice_.pairwise_means_indices.tolist() == [
-            [(2,), (), ()],
+            [(), (), (1,)],
             [(), (), ()],
         ]
         assert slice_.pairwise_significance_means_t_stats(1) == pytest.approx(
             np.array(
                 [
-                    [np.nan, np.nan, np.nan],
+                    [np.nan, 0, 2.1529364],
                     [np.nan, 0, np.nan],
                 ]
             ),
@@ -1210,7 +1222,7 @@ class TestMeanDifferenceSignificance(object):
         assert slice_.pairwise_significance_means_p_vals(1) == pytest.approx(
             np.array(
                 [
-                    [np.nan, np.nan, np.nan],
+                    [np.nan, 1, 0.04158831],
                     [np.nan, 1.0, np.nan],
                 ]
             ),
@@ -1218,7 +1230,7 @@ class TestMeanDifferenceSignificance(object):
         )
 
     def test_mean_diff_significance_indices_for_cat_x_cat(self):
-        transforms = {"pairwise_indices": {"alpha": [0.05, 0.01]}}
+        transforms = {"pairwise_indices": {"alpha": [0.15, 0.05]}}
         slice_ = Cube(CR.MEAN_CAT_X_CAT, transforms=transforms).partitions[0]
 
         assert slice_.pairwise_means_indices.tolist() == [
@@ -1262,7 +1274,7 @@ class TestMeanDifferenceSignificance(object):
             },
         ]
         transforms = {
-            "pairwise_indices": {"alpha": [0.05, 0.01]},
+            "pairwise_indices": {"alpha": [0.45, 0.255]},
             "columns_dimension": {"insertions": insertions},
         }
         slice_ = Cube(
@@ -1280,7 +1292,7 @@ class TestMeanDifferenceSignificance(object):
         ]
 
         # Test no subtotals
-        transforms = {"pairwise_indices": {"alpha": [0.05, 0.01]}}
+        transforms = {"pairwise_indices": {"alpha": [0.45, 0.255]}}
         slice_ = Cube(
             NA.NUM_ARR_MEANS_GROUPED_BY_CAT_HS_WEIGHTED, transforms=transforms
         ).partitions[0]
@@ -1302,7 +1314,7 @@ class TestMeanDifferenceSignificance(object):
                 "elements": {"1": {"hide": True}},
                 "insertions": insertions,
             },
-            "pairwise_indices": {"alpha": [0.05, 0.01]},
+            "pairwise_indices": {"alpha": [0.45, 0.255]},
         }
         slice_ = Cube(
             NA.NUM_ARR_MEANS_GROUPED_BY_CAT_HS_WEIGHTED, transforms=transforms

tests/unit/matrix/test_measure.py
@@ -11,6 +11,8 @@ from cr.cube.enums import DIMENSION_TYPE as DT, MARGINAL_ORIENTATION as MO
 from cr.cube.matrix.cubemeasure import (
     CubeMeasures,
     _BaseCubeCounts,
+    _BaseCubeMeans,
+    _BaseCubeStdDev,
     _BaseUnweightedCubeCounts,
 )
 from cr.cube.matrix.measure import (
@@ -395,6 +397,32 @@ class Describe_ColumnProportions(object):
         assert column_proportions.blocks == [[1.0, 2.0], [3.0, 4.0]]
 
 
+class Describe_ColumnShareSum(object):
+    """Unit test suite for `cr.cube.matrix.measure._ColumnShareSum` object."""
+
+    def it_computes_its_blocks(self, request):
+        SumSubtotals_ = class_mock(request, "cr.cube.matrix.measure.SumSubtotals")
+        SumSubtotals_.blocks.return_value = [
+            [[5.0, 12.0], [21.0, 32.0]],
+            [[], []],
+            [[], []],
+            [[], []],
+        ]
+        sums_blocks_ = instance_mock(request, _Sums, blocks=[[5.0, 6.0], [7.0, 8.0]])
+        second_order_measures_ = instance_mock(
+            request,
+            SecondOrderMeasures,
+            sums=sums_blocks_,
+        )
+        cube_measures_ = class_mock(request, "cr.cube.matrix.cubemeasure.CubeMeasures")
+
+        col_share_sum = _ColumnShareSum(None, second_order_measures_, cube_measures_)
+
+        assert col_share_sum.blocks[0][0] == pytest.approx([0.2941176, 0.7058823])
+        assert col_share_sum.blocks[0][1] == pytest.approx([0.3962264, 0.6037735])
+        SumSubtotals_.blocks.assert_called_once_with(ANY, None, True, True)
+
+
 class Describe_ColumnUnweightedBases(object):
     """Unit test suite for `cr.cube.matrix.measure._ColumnUnweightedBases` object."""
 
@@ -616,6 +644,30 @@ class Describe_ColumnWeightedBases(object):
     @pytest.fixture
     def _weighted_cube_counts_prop_(self, request):
         return property_mock(request, _ColumnWeightedBases, "_weighted_cube_counts")
+
+
+class Describe_PairwiseMeansSigTStats(object):
+    """Unit test suite for `cr.cube.matrix.measure._PairwiseMeansSigTStats` object."""
+
+    def it_provides_the_tstats(self, request):
+        # Adapted from:
+        # https://mse.redwoods.edu/darnold/math15/spring2013/R/Activities/WelchTTest.html
+        cube_means_ = instance_mock(
+            request, _BaseCubeMeans, means=np.array([[41.52174, 51.47619]])
+        )
+        cube_std_dev_ = instance_mock(
+            request, _BaseCubeStdDev, stddev=np.array([[17.14873, 11.00736]])
+        )
+        unweighted_cube_counts_ = instance_mock(
+            request, _BaseCubeCounts, column_bases=np.array([[23, 21]])
+        )
+        cube_measures_ = instance_mock(request, CubeMeasures)
+        cube_measures_.cube_means = cube_means_
+        cube_measures_.cube_stddev = cube_std_dev_
+        cube_measures_.unweighted_cube_counts = unweighted_cube_counts_
+        pairwise_tstat = _PairwiseMeansSigTStats(None, None, cube_measures_, 0)
+
+        assert pairwise_tstat.t_stats == pytest.approx(np.array([[0, 2.310889]]))
 
 
 class Describe_PopulationProportions(object):
@@ -1008,32 +1060,6 @@ class Describe_RowWeightedBases(object):
     @pytest.fixture
     def _weighted_cube_counts_prop_(self, request):
         return property_mock(request, _RowWeightedBases, "_weighted_cube_counts")
-
-
-class Describe_ColumnShareSum(object):
-    """Unit test suite for `cr.cube.matrix.measure._ColumnShareSum` object."""
-
-    def it_computes_its_blocks(self, request):
-        SumSubtotals_ = class_mock(request, "cr.cube.matrix.measure.SumSubtotals")
-        SumSubtotals_.blocks.return_value = [
-            [[5.0, 12.0], [21.0, 32.0]],
-            [[], []],
-            [[], []],
-            [[], []],
-        ]
-        sums_blocks_ = instance_mock(request, _Sums, blocks=[[5.0, 6.0], [7.0, 8.0]])
-        second_order_measures_ = instance_mock(
-            request,
-            SecondOrderMeasures,
-            sums=sums_blocks_,
-        )
-        cube_measures_ = class_mock(request, "cr.cube.matrix.cubemeasure.CubeMeasures")
-
-        col_share_sum = _ColumnShareSum(None, second_order_measures_, cube_measures_)
-
-        assert col_share_sum.blocks[0][0] == pytest.approx([0.2941176, 0.7058823])
-        assert col_share_sum.blocks[0][1] == pytest.approx([0.3962264, 0.6037735])
-        SumSubtotals_.blocks.assert_called_once_with(ANY, None, True, True)
 
 
 class Describe_RowShareSum(object):