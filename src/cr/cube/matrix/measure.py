[180351052]: use correct variance formula for col prop diffs

src/cr/cube/matrix/measure.py
@@ -10,6 +10,8 @@ from cr.cube.matrix.cubemeasure import CubeMeasures
 from cr.cube.matrix.subtotals import (
     SumSubtotals,
     NanSubtotals,
+    NegativeTermSubtotals,
+    PositiveTermSubtotals,
 )
 from cr.cube.smoothing import Smoother
 from cr.cube.util import lazyproperty
@@ -793,7 +795,19 @@ class _ColumnProportionsSmoothed(_ColumnProportions, _SmoothedMeasure):
 class _ColumnProportionVariances(_BaseSecondOrderMeasure):
     """Provides the variance of the column-proportions measure for a matrix.
 
-    Column-proportions-variance is a 2D np.float64 ndarray of p * (1 - p) where p is the
+    Column-proportions-variance is a 2D np.float64 ndarray.  When there are
+    negative terms, the full formula is:
+    (1 - p)^2 * (Np / Nt) + (0 - p)^2 * (Ni / Nt) + (-1 - p)^2 * (Nn / Nt)
+
+    Where:
+    p = column-proportions (eg (Np - Nn) / Nt))
+    Np = weighted-count of positive terms
+    Ni = weighted-count of "ignored" (eg not negative nor positive) terms
+    Nn = weighted-count of negative terms
+    Nt = weighted-base (sum of all counts eg Np + Ni + Nn)
+
+    When there are no negative terms (eg not subtotal differences), the formula
+    can be simplified to the more familiar p * (1 - p) where p is the
     column-proportions.
     """
 
@@ -805,21 +819,78 @@ class _ColumnProportionVariances(_BaseSecondOrderMeasure):
         subtotal intersection-cell values.
         """
         p = self._second_order_measures.column_proportions.blocks
+        Nt = self._count_total
+        Np = self._count_positive
+        Ni = self._count_ignored
+        Nn = self._count_negative
 
         return [
             [
                 # --- base values ---
-                p[0][0] * (1 - p[0][0]),
+                self._calc_var(p[0][0], Nt[0][0], Np[0][0], Ni[0][0], Nn[0][0]),
                 # --- inserted columns ---
-                p[0][1] * (1 - p[0][1]),
+                self._calc_var(p[0][1], Nt[0][1], Np[0][1], Ni[0][1], Nn[0][1]),
             ],
             [
                 # --- inserted rows ---
-                p[1][0] * (1 - p[1][0]),
+                self._calc_var(p[1][0], Nt[1][0], Np[1][0], Ni[1][0], Nn[1][0]),
                 # --- intersections ---
-                p[1][1] * (1 - p[1][1]),
+                self._calc_var(p[1][1], Nt[1][1], Np[1][1], Ni[1][1], Nn[1][1]),
             ],
         ]
+
+    def _calc_var(self, p, Nt, Np, Ni, Nn):
+        """2D ndarray of float64 representing variance.
+
+        See the class docstring for details on the formula.
+        """
+        p_term = ((1 - p) ** 2) * (Np / Nt)
+        i_term = ((0 - p) ** 2) * (Ni / Nt)
+        n_term = ((-1 - p) ** 2) * (Nn / Nt)
+        return p_term + i_term + n_term
+
+    @lazyproperty
+    def _count_ignored(self):
+        """blocks of 2D np.float64 ndarray of the count of ignored
+
+        The ignored values are the ones that are included in neither the positive
+        term nor the negative term.
+        """
+        negative = self._count_negative
+        positive = self._count_positive
+        total = self._count_total
+
+        return [
+            [
+                total[0][0] - positive[0][0] - negative[0][0],
+                total[0][1] - positive[0][1] - negative[0][1],
+            ],
+            [
+                total[1][0] - positive[1][0] - negative[1][0],
+                total[1][1] - positive[1][1] - negative[1][1],
+            ],
+        ]
+
+    @lazyproperty
+    def _count_negative(self):
+        """blocks of 2D np.float64 ndarray of the count of subtracted values"""
+        return NegativeTermSubtotals.blocks(
+            self._weighted_cube_counts.counts,
+            self._dimensions,
+        )
+
+    @lazyproperty
+    def _count_positive(self):
+        """blocks of 2D np.float64 ndarray of the count of positive values"""
+        return PositiveTermSubtotals.blocks(
+            self._weighted_cube_counts.counts,
+            self._dimensions,
+        )
+
+    @lazyproperty
+    def _count_total(self):
+        """blocks of 2D np.float64 ndarray of the count of the base"""
+        return self._second_order_measures.column_weighted_bases.blocks
 
 
 class _ColumnShareSum(_BaseSecondOrderMeasure):

tests/integration/test_headers_and_subtotals.py
@@ -2838,19 +2838,19 @@ class DescribeIntegrated_SubtotalDifferences(object):
             np.full(5, np.nan), nan_ok=True
         )
         assert slice_.column_proportions_moe[0, :] == pytest.approx(
-            [np.nan, np.nan, 0, np.nan, np.nan], nan_ok=True
+            [np.nan, 0.1731749, 0.18519919, 0.13934182, 0.18081228], nan_ok=True
         )
         assert slice_.column_std_dev[:, 0] == pytest.approx(
             np.full(5, np.nan), nan_ok=True
         )
         assert slice_.column_std_dev[0, :] == pytest.approx(
-            [np.nan, np.nan, 0, np.nan, np.nan], nan_ok=True
+            [np.nan, 0.72322635, 0.70710678, 0.63189792, 0.73802285], nan_ok=True
         )
         assert slice_.column_std_err[:, 0] == pytest.approx(
             np.full(5, np.nan), nan_ok=True
         )
         assert slice_.column_std_err[0, :] == pytest.approx(
-            [np.nan, np.nan, 0, np.nan, np.nan], nan_ok=True
+            [np.nan, 0.08835616, 0.09449112, 0.07109407, 0.09225286], nan_ok=True
         )
         assert slice_.row_proportions[0, :] == pytest.approx(
             np.full(5, np.nan), nan_ok=True

tests/unit/matrix/test_measure.py
@@ -66,6 +66,7 @@ from cr.cube.matrix.measure import (
     _WeightedCounts,
     _Zscores,
 )
+from cr.cube.matrix.subtotals import NegativeTermSubtotals, PositiveTermSubtotals
 
 from ...unitutil import ANY, call, class_mock, instance_mock, method_mock, property_mock
 
@@ -405,22 +406,129 @@ class Describe_ColumnProportions:
 class Describe_ColumnProportionVariances:
     """Unit test suite for `cr.cube.matrix.measure._ColumnProportionVariances` object."""
 
-    def it_computes_its_blocks(self, request):
+    def it_computes_its_blocks(
+        self,
+        request,
+        second_order_measures_,
+        _count_total_prop_,
+        _count_positive_prop_,
+        _count_negative_prop_,
+        _count_ignored_prop_,
+    ):
         column_proportions_ = instance_mock(
-            request, _ColumnProportions, blocks=[[0.1, 0.3], [0.25, 0.35]]
+            request, _ColumnProportions, blocks=[["p1", "p2"], ["p3", "p4"]]
         )
-        second_order_measures_ = instance_mock(
+        second_order_measures_.column_proportions = column_proportions_
+        _count_total_prop_.return_value = [["Nt1", "Nt2"], ["Nt3", "Nt4"]]
+        _count_positive_prop_.return_value = [["Np1", "Np2"], ["Np3", "Np4"]]
+        _count_ignored_prop_.return_value = [["Ni1", "Ni2"], ["Ni3", "Ni4"]]
+        _count_negative_prop_.return_value = [["Nn1", "Nn2"], ["Nn3", "Nn4"]]
+        _calc_var_ = method_mock(
             request,
-            SecondOrderMeasures,
-            column_proportions=column_proportions_,
+            _ColumnProportionVariances,
+            "_calc_var",
+            side_effect=("a", "b", "c", "d"),
         )
-
         variances = _ColumnProportionVariances(None, second_order_measures_, None)
 
-        assert variances.blocks == [
-            pytest.approx([0.09, 0.21]),
-            pytest.approx([0.1875, 0.2275]),
+        assert variances.blocks == [["a", "b"], ["c", "d"]]
+        assert _calc_var_.call_args_list == [
+            call(variances, "p1", "Nt1", "Np1", "Ni1", "Nn1"),
+            call(variances, "p2", "Nt2", "Np2", "Ni2", "Nn2"),
+            call(variances, "p3", "Nt3", "Np3", "Ni3", "Nn3"),
+            call(variances, "p4", "Nt4", "Np4", "Ni4", "Nn4"),
         ]
+
+    def it_can_compute_variance_to_help(self):
+        p = np.array([[0.8, 0.3], [0.2, 0.7], [0.6, -0.4]])
+        Nt = np.array([[5, 10], [5, 10], [5, 10]])
+        Np = np.array([[4, 3], [1, 7], [4, 3]])
+        Ni = np.array([[1, 7], [4, 3], [0, 0]])
+        Nn = np.array([[0, 0], [0, 0], [1, 7]])
+        variances = _ColumnProportionVariances(None, None, None)
+
+        assert variances._calc_var(p, Nt, Np, Ni, Nn).tolist() == [
+            pytest.approx([0.16, 0.21]),
+            pytest.approx([0.16, 0.21]),
+            pytest.approx([0.64, 0.84]),
+        ]
+
+    def it_provides_the_count_ignored_to_help(
+        self, _count_total_prop_, _count_positive_prop_, _count_negative_prop_
+    ):
+        _count_total_prop_.return_value = [[5, 10], [6, 11]]
+        _count_positive_prop_.return_value = [[1, 4], [2, 9]]
+        _count_negative_prop_.return_value = [[0, 2], [1, 1]]
+        variances = _ColumnProportionVariances(None, None, None)
+
+        assert variances._count_ignored == [[4, 4], [3, 1]]
+
+    def it_provides_the_count_negative_to_help(
+        self, request, dimensions_, _weighted_cube_counts_prop_, cube_counts_
+    ):
+        negative_term_blocks = method_mock(
+            request, NegativeTermSubtotals, "blocks", return_value="b"
+        )
+        _weighted_cube_counts_prop_.return_value = cube_counts_
+        variances = _ColumnProportionVariances(dimensions_, None, None)
+
+        assert variances._count_negative == "b"
+        negative_term_blocks.assert_called_once_with(cube_counts_.counts, dimensions_)
+
+    def it_provides_the_count_positive_to_help(
+        self, request, dimensions_, _weighted_cube_counts_prop_, cube_counts_
+    ):
+        positive_term_blocks = method_mock(
+            request, PositiveTermSubtotals, "blocks", return_value="b"
+        )
+        _weighted_cube_counts_prop_.return_value = cube_counts_
+        variances = _ColumnProportionVariances(dimensions_, None, None)
+
+        assert variances._count_positive == "b"
+        positive_term_blocks.assert_called_once_with(cube_counts_.counts, dimensions_)
+
+    def it_provides_the_count_total_to_help(self, request, second_order_measures_):
+        column_weighted_bases_ = instance_mock(request, _ColumnWeightedBases)
+        second_order_measures_.column_weighted_bases = column_weighted_bases_
+        variances = _ColumnProportionVariances(None, second_order_measures_, None)
+
+        assert variances._count_total == column_weighted_bases_.blocks
+
+    # fixture components ---------------------------------------------
+
+    @pytest.fixture
+    def _count_ignored_prop_(self, request):
+        return property_mock(request, _ColumnProportionVariances, "_count_ignored")
+
+    @pytest.fixture
+    def _count_negative_prop_(self, request):
+        return property_mock(request, _ColumnProportionVariances, "_count_negative")
+
+    @pytest.fixture
+    def _count_positive_prop_(self, request):
+        return property_mock(request, _ColumnProportionVariances, "_count_positive")
+
+    @pytest.fixture
+    def _count_total_prop_(self, request):
+        return property_mock(request, _ColumnProportionVariances, "_count_total")
+
+    @pytest.fixture
+    def cube_counts_(self, request):
+        return instance_mock(request, _BaseCubeCounts)
+
+    @pytest.fixture
+    def dimensions_(self, request):
+        return (instance_mock(request, Dimension), instance_mock(request, Dimension))
+
+    @pytest.fixture
+    def second_order_measures_(self, request):
+        return instance_mock(request, SecondOrderMeasures)
+
+    @pytest.fixture
+    def _weighted_cube_counts_prop_(self, request):
+        return property_mock(
+            request, _ColumnProportionVariances, "_weighted_cube_counts"
+        )
 
 
 class Describe_ColumnShareSum: