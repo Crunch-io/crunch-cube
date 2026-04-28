6f162ddbd21354d2e0e04d40a7dbcc4f8260d539
Greg Freedman Ellis <greg@crunch.io>
2026-04-28 10:37:15 -0500

    remove outdated comment about py38

--- src/cr/cube/matrix/measure.py
+++ src/cr/cube/matrix/measure.py
@@ -2793,8 +2793,6 @@ class _DisaggregatedMissings(_BaseMarginal):
     @lazyproperty
     def _inner_dtype(self):
         """numpy.dtype used for items in the arrays of tuples in blocks"""
-        # --- Note that the string notation for a tuple of length 1 ("d,")
-        # --- does not work on all versions of numpy we support (python 3.8 testrunners)
         return np.dtype([(f"f{i}", np.float64) for i in range(self._inner_size)])
 
     @lazyproperty
